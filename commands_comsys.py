86e9a8292c64683e3073a7ff1ebd3e59a8582012
Michael King <mking@arikel.net>
2007-05-04 18:02:49 +0000

    * added skeletons for commands_comsys.py (has all of the MUX2 commands I * could remember) and functions_comsys.py (has a couple of ideas for * useful functions).

---
+++ commands_comsys.py
@@ -0,0 +1,147 @@
+import settings
+import time
+import functions_general
+import functions_db
+import functions_help
+import defines_global as global_defines
+import session_mgr
+import ansi
+import os
+"""
+Comsys command module. Pretty much every comsys command should go here for
+now.
+"""   
+def cmd_addcom(cdat):
+  """
+  addcom
+
+  Adds an alias for a channel.
+  addcom foo=Bar
+  """
+  pass
+
+def cmd_allcom(cdat):
+  """
+  allcom
+
+  Allows the user to universally turn off or on all channels they are on, 
+  as well as perform a "who" for all channels they are on.
+  """
+  pass
+
+def cmd_comtitle(cdat):
+  """
+  comtitle
+
+  Sets a prefix to the user's name on the specified channel.
+  """
+  pass
+
+def cmd_clearcom(cdat):
+  """
+  clearcom
+
+  Effectively runs delcom on all channels the user is on.  It will remove their aliases,
+  remove them from the channel, and clear any titles they have set.
+  """
+  pass
+
+def cmd_clist(cdat):
+  """
+  @clist
+
+  Lists all available channels on the game.
+  """
+  pass
+
+def cmd_ccreate(cdat):
+  """
+  @ccreate
+
+  Creates a channel.
+  """
+  pass
+
+def cmd_cdestroy(cdat):
+  """
+  @cdestroy
+
+  Destroys a channel.
+  """
+  pass
+
+def cmd_cset(cdat):
+  """
+  @cset
+
+  Sets various flags on a channel.
+  """
+  pass
+
+def cmd_cpflags(cdat):
+  """
+  @cpflags
+
+  Sets various flags on a channel relating to players.
+  """
+  pass
+
+def cmd_coflags(cdat):
+  """
+  @coflags
+
+  Sets various flags on a channel relating to objects.
+  """
+  pass
+
+def cmd_ccharge(cdat):
+  """
+  @ccharge
+
+  Sets the cost to transmit over a channel.  Default is free.
+  """
+  pass
+
+def cmd_cboot(cdat):
+  """
+  @cboot
+
+  Kicks a player or object from the channel.
+  """
+  pass
+
+def cmd_cemit(cdat):
+  """
+  @cemit
+
+  Allows the user to send a message over a channel as long as
+  they own or control it.  It does not show the user's name.
+  """
+  pass
+
+def cmd_cwho(cdat):
+  """
+  @cwho
+
+  Displays the name, status and object type for a given channel.
+  Adding /all after the channel name will list disconnected players
+  as well.
+  """
+  pass
+
+def cmd_cchown(cdat):
+  """
+  @cchown
+
+  Changes the owner of a channel.
+  """
+  pass
+
+def cmd_delcom(cdat):
+  """
+  delcom
+
+  Removes the specified alias to a channel.  If this is the last alias,
+  the user is effectively removed from the channel.
+  """
+  pass
---
+++ functions_comsys.py
@@ -0,0 +1,23 @@
+import session_mgr
+import commands_privileged
+import commands_general
+import commands_comsys
+import commands_unloggedin
+"""
+Comsys functions.
+"""
+
+def set_new_title(channel, player, title):
+  pass
+
+def get_com_who(channel, muted=False, disconnected=False):
+  """
+  Get all users on a channel.
+
+  If muted = True, return users who have it muted as well.
+  If disconnected = True, return users who are not connected as well.
+  """
+  pass
+
+def get_user_channels(player):
+  pass
