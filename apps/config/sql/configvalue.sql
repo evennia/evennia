543a9cfe265733ff2808082acc137531e75df1fb
Greg Taylor <snagglepants@gmail.com>
2007-05-27 21:56:40 +0000

    Fix for MySQL sillyness.

--- apps/config/sql/configvalue.sql
+++ apps/config/sql/configvalue.sql
@@ -1,7 +1,7 @@
-INSERT INTO config_configvalue VALUES(0,'site_name','Evennia Test Site');
-INSERT INTO config_configvalue VALUES(1,'site_port','4000');
-INSERT INTO config_configvalue VALUES(2,'player_dbnum_start','2');
-INSERT INTO config_configvalue VALUES(3,'money_name_plural','Credits');
-INSERT INTO config_configvalue VALUES(4,'money_name_singular','Credit');
-INSERT INTO config_configvalue VALUES(5,'game_firstrun','1');
-INSERT INTO config_configvalue VALUES(6,'idle_timeout','1800');
+INSERT INTO config_configvalue VALUES(1,'site_name','Evennia Test Site');
+INSERT INTO config_configvalue VALUES(2,'site_port','4000');
+INSERT INTO config_configvalue VALUES(3,'player_dbnum_start','2');
+INSERT INTO config_configvalue VALUES(4,'money_name_plural','Credits');
+INSERT INTO config_configvalue VALUES(5,'money_name_singular','Credit');
+INSERT INTO config_configvalue VALUES(6,'game_firstrun','1');
+INSERT INTO config_configvalue VALUES(7,'idle_timeout','1800');
