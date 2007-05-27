f7d58f16ae5a4905a1304b598a6dabf330ff9e3d
Greg Taylor <snagglepants@gmail.com>
2007-05-27 20:45:40 +0000

    Possible MySQL fix.

--- apps/helpsys/sql/helpentry.sql
+++ apps/helpsys/sql/helpentry.sql
@@ -1,14 +1,14 @@
-INSERT INTO "helpsys_helpentry" VALUES(1,'Help Index','This game has yet to customize its help index, so for now you may browse the generic codebase help files.
+INSERT INTO helpsys_helpentry VALUES(1,'Help Index','This game has yet to customize its help index, so for now you may browse the generic codebase help files.
 
 Topics
 ------
 NEWBIE %%t%%t Getting started (for new players).
 COMMANDS %%t How to get help with commands.
 CREDITS %%t Codebase credits.',0);
-INSERT INTO "helpsys_helpentry" VALUES(2,'Credits','Evennia is a product of a small community of developers, all working towards the continual improvement of the codebase. The following people have made major contributions with the end result being what you are now playing.
+INSERT INTO helpsys_helpentry VALUES(2,'Credits','Evennia is a product of a small community of developers, all working towards the continual improvement of the codebase. The following people have made major contributions with the end result being what you are now playing.
 
 "Kelvin" (Greg Taylor) - Lead developer and original author.',0);
-INSERT INTO "helpsys_helpentry" VALUES(3,'Commands','Commands in Evennia are generally organized into one of two categories: %%cgPublic%%cn or %%cyPrivileged%%cn commands.
+INSERT INTO helpsys_helpentry VALUES(3,'Commands','Commands in Evennia are generally organized into one of two categories: %%cgPublic%%cn or %%cyPrivileged%%cn commands.
 
 %%cgPublic%%cn commands are more or less available to everyone. None of these commands are prefixed with anything, they are typical, every-day commands like %%chlook%%cn, %%chsay%%cn, and %%chget%%cn.
 
