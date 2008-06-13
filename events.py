740d715c7277a5bb8e5548b52f93181b9f4b79f3
loki77 <loki77@gmail.com>
2008-06-13 19:41:23 +0000

    Modified for 4 space indentation.

--- events.py
+++ events.py
@@ -5,7 +5,7 @@ Holds the events scheduled in scheduler.py.
 """
 
 def evt_check_sessions():
-   """
-   Event: Check all of the connected sessions.
-   """
-   session_mgr.check_all_sessions()
+    """
+    Event: Check all of the connected sessions.
+    """
+    session_mgr.check_all_sessions()
