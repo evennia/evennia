f1760e6521164e963b7f049cb190629030e6091e
Greg Taylor <snagglepants@gmail.com>
2008-06-14 02:26:40 +0000

    I guess it helps if I actually add the file.

---
+++ settings_common.py
@@ -0,0 +1,49 @@
+"""
+Common settings that don't change very much. This allows us to update things
+like apps through subversion without developers having to modify their
+settings.py file, which is not versioned.
+"""
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+# Context processors define context variables, generally for the template
+# system to use.
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'apps.website.webcontext.general_context',
+)
+
+# MiddleWare are semi-transparent extensions to Django's functionality.
+# see http://www.djangoproject.com/documentation/middleware/ for a more detailed
+# explanation.
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+)
+
+# Global and Evennia-specific apps. This ties everything together so we can
+# refer to app models and perform DB syncs.
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.sites',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'django.contrib.flatpages',
+    'apps.config',
+    'apps.objects',
+    'apps.helpsys',
+    'apps.genperms',
+    'apps.news',
+    'apps.website',
+)
