214534a86fe8a7113cfc56255e201f5500744215
Greg Taylor <snagglepants@gmail.com>
2009-01-22 04:01:54 +0000

    Fix the Auth section not showing up in automatic admin.

--- game/web/urls.py
+++ game/web/urls.py
@@ -10,7 +10,7 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
 
-#admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # User Authentication
