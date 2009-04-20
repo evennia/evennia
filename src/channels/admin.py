5ec19c264541f93eea101e5fe1d92095fb718bb6
Greg Taylor <snagglepants@gmail.com>
2009-04-20 02:45:40 +0000

    Actually, I did mess up and not commit the channels app. Yick. Fixed.

---
+++ src/channels/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from src.channels.models import CommChannel, CommChannelMessage
+
+class CommChannelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner')
+admin.site.register(CommChannel, CommChannelAdmin)
+
+class CommChannelMessageAdmin(admin.ModelAdmin):
+    list_display = ('channel', 'message')
+admin.site.register(CommChannelMessage, CommChannelMessageAdmin)
