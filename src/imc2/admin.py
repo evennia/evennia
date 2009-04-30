6c5488aab3d6d9eb3d898b482d559e6b82330535
Greg Taylor <snagglepants@gmail.com>
2009-04-30 04:21:58 +0000

    Show the IMC server name for IMC2ChannelMapping objects in the Django admin interface.

--- src/imc2/admin.py
+++ src/imc2/admin.py
@@ -2,5 +2,6 @@ from src.imc2.models import IMC2ChannelMapping
 from django.contrib import admin
 
 class IMC2ChannelMappingAdmin(admin.ModelAdmin):
-    list_display = ('channel', 'imc2_channel_name', 'is_enabled')
+    list_display = ('channel', 'imc2_server_name',
+                    'imc2_channel_name', 'is_enabled')
 admin.site.register(IMC2ChannelMapping, IMC2ChannelMappingAdmin)
