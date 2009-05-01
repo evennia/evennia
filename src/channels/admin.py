119f5f6d49a8dfc8551bde1f592a2a5a9f1446bd
Greg Taylor <snagglepants@gmail.com>
2009-05-01 05:07:13 +0000

    Show a little more info in the Django admin for comm channels.

--- src/channels/admin.py
+++ src/channels/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from src.channels.models import CommChannel, CommChannelMessage
 
 class CommChannelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner')
+    list_display = ('name', 'ansi_name', 'owner', 'description', 'is_joined_by_default')
 admin.site.register(CommChannel, CommChannelAdmin)
 
 class CommChannelMessageAdmin(admin.ModelAdmin):
