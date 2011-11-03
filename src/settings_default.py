ce00d7c920d925d3b44f5de0e95e340fef96b9cb
Griatch <griatch@gmail.com>
2011-11-03 22:55:12 +0100

    LOCALE: Added Swedish translation.

--- src/settings_default.py
+++ src/settings_default.py
@@ -343,7 +343,9 @@ SESSION_COOKIE_NAME = 'sessionid'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
+# Where to find locales (no need to change this, most likely)
+LOCALE_PATHS = ["../locale/"]
 # This should be turned off unless you want to do tests with Django's 
 # development webserver (normally Evennia runs its own server)
 SERVE_MEDIA = False
