5e8f077eb57902f78038ebae0f7a4c7d11e0971e
lagos <zeta142125@gmail.com>
2012-09-27 16:50:11 -0700

    Fixes clobbering of 'path' argument.

--- contrib/procpools/ampoule/main.py
+++ contrib/procpools/ampoule/main.py
@@ -278,8 +278,8 @@ def spawnProcess(processProtocol, bootstrap, args=(), env={},
 
     pythonpath = []
     for pkg in packages:
-        path, name = os.path.split(pkg)
-        p = os.path.split(imp.find_module(name, [path] if path else None)[1])[0]
+        pkg_path, name = os.path.split(pkg)
+        p = os.path.split(imp.find_module(name, [pkg_path] if pkg_path else None)[1])[0]
         if p.startswith(os.path.join(sys.prefix, 'lib')):
             continue
         pythonpath.append(p)
