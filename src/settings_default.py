16d49e61d3fb1a12df955ff1f7d29d70e8a17c4e
Griatch <griatch@gmail.com>
2012-10-24 00:18:09 +0200

    Added MessageMiddleware, which is required by the News site on the website. A Django 1.4 change? This was highlighted as part of Issue 249.

--- src/settings_default.py
+++ src/settings_default.py
@@ -431,6 +431,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware', # 1.4?
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.doc.XViewMiddleware',
