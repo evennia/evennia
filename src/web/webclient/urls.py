9d0b8b5fe7dd4a5265eea34522cadb21969c2bba
Kelketek <kelketek@gmail.com>
2013-03-23 14:20:41 -0500

    Added compatibility with Django 1.5. New minimum version is 1.4.

--- src/web/webclient/urls.py
+++ src/web/webclient/urls.py
@@ -2,9 +2,7 @@
 This structures the (simple) structure of the 
 webpage 'application'. 
 """
-from django.views.generic.simple import direct_to_template
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('',
    url(r'^$', 'src.web.webclient.views.webclient'),)
-   #url(r'^$', direct_to_template, {'template': 'webclient.html'}),)
