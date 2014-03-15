440afe692864c8deb5b9ed2c1c6266f1b0a4d681
Griatch <griatch@gmail.com>
2014-03-15 14:06:28 +0100

    Further tweaks to unit test systems

--- src/server/tests.py
+++ src/server/tests.py
@@ -26,15 +26,35 @@ try:
 except ImportError:
     import unittest
 
-from src.locks import tests as locktests
-from src.utils import tests as utiltests
-from src.commands.default import tests as commandtests
+from django.conf import settings
+from django.test.simple import DjangoTestSuiteRunner
+
+
+class EvenniaTestSuiteRunner(DjangoTestSuiteRunner):
+    """
+    This test runner only runs tests on the apps specified in src/ and game/ to
+     avoid running the large number of tests defined by Django
+    """
+    def build_suite(self, test_labels, extra_tests=None, **kwargs):
+        """
+        Build a test suite for Evennia. test_labels is a list of apps to test.
+        If not given, a subset of settings.INSTALLED_APPS will be used.
+        """
+        if not test_labels:
+            test_labels = [applabel.rsplit('.', 1)[1] for applabel in settings.INSTALLED_APPS
+                           if (applabel.startswith('src.') or applabel.startswith('game.'))]
+        return super(EvenniaTestSuiteRunner, self).build_suite(test_labels, extra_tests=extra_tests, **kwargs)
+
 
 def suite():
     """
     This function is called automatically by the django test runner.
     This also collates tests from packages that are not formally django applications.
     """
+    from src.locks import tests as locktests
+    from src.utils import tests as utiltests
+    from src.commands.default import tests as commandtests
+
     tsuite = unittest.TestSuite()
     tsuite.addTest(unittest.defaultTestLoader.loadTestsFromModule(sys.modules[__name__]))
 
