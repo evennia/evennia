a749f0c5caf2a92323ad5cd2cd54dabfb6c176a6
Griatch <griatch@gmail.com>
2014-09-10 08:58:21 +0200

    Made unloggedin help reflect the avilability of the encoding command also at this stage.

--- src/commands/default/unloggedin.py
+++ src/commands/default/unloggedin.py
@@ -303,25 +303,27 @@ class CmdUnconnectedHelp(MuxCommand):
         string = \
             """
 You are not yet logged into the game. Commands available at this point:
-  {wcreate, connect, look, help, quit{n
 
-To login to the system, you need to do one of the following:
+  {wcreate{n - create a new account
+  {wconnect{n - connect with an existing account
+  {wlook{n - re-show the connection screen
+  {whelp{n - show this help
+  {wencoding{n - change the text encoding to match your client
+  {wquit{n - abort the connection
 
-{w1){n If you have no previous account, you need to use the 'create'
-   command.
+To login, first create an account
 
      {wcreate Anna c67jHL8p{n
 
-   Note that if you use spaces in your name, you have to enclose in quotes.
+   Note that if you use spaces in your name, you have to enclose in quotes:
 
      {wcreate "Anna the Barbarian"  c67jHL8p{n
 
    It's always a good idea (not only here, but everywhere on the net)
    to not use a regular word for your password. Make it longer than
-   6 characters or write a passphrase.
+   6 characters or write a full passphrase.
 
-{w2){n If you have an account already, either because you just created
-   one in {w1){n above or you are returning, use the 'connect' command:
+Once you have an account, connect using your password
 
      {wconnect Anna c67jHL8p{n
 
@@ -330,6 +332,7 @@ To login to the system, you need to do one of the following:
    to get more aid. Hope you enjoy your stay!
 
 You can use the {wlook{n command if you want to see the connect screen again.
+
 """
         self.caller.msg(string)
 
@@ -339,7 +342,7 @@ class CmdUnconnectedEncoding(MuxCommand):
     set which text encoding to use in unconnected-in state
 
     Usage:
-      @encoding/switches [<encoding>]
+      encoding/switches [<encoding>]
 
     Switches:
       clear - clear your custom encoding
@@ -357,8 +360,8 @@ class CmdUnconnectedEncoding(MuxCommand):
     instead.
   """
 
-    key = "@encoding"
-    aliases = "@encode"
+    key = "encoding"
+    aliases = "@encoding, @encode"
     locks = "cmd:all()"
 
     def func(self):
