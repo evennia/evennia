f075bcf2973e19cbbb29dfcb6376cfd8f4a2d78f
Jonathan Piacenti <kelketek@gmail.com>
2015-02-22 17:19:05 -0600

    Remove unused test stubs.

--- evennia/tests/test_utils_ansi_new.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestSubMeth(unittest.TestCase):
-    def test_sub_meth(self):
-        # self.assertEqual(expected, sub_meth(obj, function))
-        assert True # TODO: implement your test here
-
-if __name__ == '__main__':
-    unittest.main()
