4e953314128b42ef473c9e71cbe83bdf933fbbdd
Jonathan Piacenti <kelketek@gmail.com>
2015-02-23 18:17:16 -0600

    Created base collab test class.

--- evennia/contrib/collab/test_base.py
+++ evennia/contrib/collab/test_base.py
@@ -1,29 +1,29 @@
-from copy import deepcopy
-from contrib.collab import collab_settings
-from contrib.collab.typeclasses import CollabCharacter, CollabObject, CollabPlayer
-from django.conf import settings
-from src.commands.default.tests import CommandTest
-from src.locks.lockhandler import _cache_lockfuncs
+from django.test import override_settings
+from evennia.contrib.collab import collab_settings
+from evennia.contrib.collab.typeclasses import CollabCharacter, CollabObject, CollabAccount, CollabExit
+from evennia.commands.default.tests import CommandTest
+from evennia.locks.lockhandler import _cache_lockfuncs
+from evennia import settings_default
 
 
+collab_overrides = {key: value for key, value in collab_settings.__dict__.items() if key.isupper()}
+collab_overrides['LOCK_FUNC_MODULES'] = settings_default.LOCK_FUNC_MODULES + ('evennia.contrib.collab.locks',)
+
+
+@override_settings(**collab_overrides)
 class CollabTest(CommandTest):
     character_typeclass = CollabCharacter
     object_typeclass = CollabObject
-    player_typeclass = CollabPlayer
+    account_typeclass = CollabAccount
+    exit_typeclass = CollabExit
 
     def setUp(self):
         """
-        Some tests will involve manipulating quota number settings.
+        The lock functions will be overwritten, so we'll need to recache them.
         """
-        self.old_types = collab_settings.CREATE_TYPES
-        collab_settings.CREATE_TYPES = deepcopy(self.old_types)
-        self.types = collab_settings.CREATE_TYPES
-        settings.LOCK_FUNC_MODULES += ('contrib.collab.locks',)
         _cache_lockfuncs()
         super(CollabTest, self).setUp()
 
     def tearDown(self):
-        collab_settings.CREATE_TYPES = self.old_types
-        settings.LOCK_FUNC_MODULES = settings.LOCK_FUNC_MODULES[:-1]
         _cache_lockfuncs()
         super(CollabTest, self).tearDown()
