6401e6182bd66e43c9b30f2f355f7bc975555d94
Griatch <griatch@gmail.com>
2015-03-09 13:51:18 +0100

    Added package info for docs

--- evennia/contrib/__init__.py
+++ evennia/contrib/__init__.py
@@ -5,3 +5,7 @@ useful but are deemed too game-specific to go into the core library.
 
 See README.md for more info.
 """
+# imports for apidoc
+import barter, dice, extended_room, menu_login, talking_npc
+import chargen, email-login, genderub, lineeditor, menusystem, slow_exit
+import tutorial_world, tutorial_examples
