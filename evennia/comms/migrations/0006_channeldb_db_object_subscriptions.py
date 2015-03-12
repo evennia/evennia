94778f1c44b4980738dfb91a4132ae549f51c9fb
Griatch <griatch@gmail.com>
2015-03-12 20:28:17 +0100

    Run migrations! Made Channel model also accept ObjectDB subscriptions.

---
+++ evennia/comms/migrations/0006_channeldb_db_object_subscriptions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('objects', '0004_auto_20150118_1622'),
+        ('comms', '0005_auto_20150223_1517'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='channeldb',
+            name='db_object_subscriptions',
+            field=models.ManyToManyField(related_name='object_subscription_set', null=True, verbose_name=b'subscriptions', to='objects.ObjectDB', db_index=True),
+            preserve_default=True,
+        ),
+    ]
