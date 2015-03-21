247ef4f2075ef6a30e810ec1ff2e26e516b2580f
tajmone <tajmone@libero.it>
2015-03-21 23:44:59 +0100

    Typos in example_batch_cmds.py

--- evennia/contrib/tutorial_examples/example_batch_code.py
+++ evennia/contrib/tutorial_examples/example_batch_code.py
@@ -2,9 +2,9 @@
 # Batchcode script
 #
 #
-# The Batch-code processor accepts full python modules (e.g. "batch.py") that
-# looks identical to normal Python files with a few exceptions that allows them
-# to the executed in blocks. This way of working assures a sequential execution
+# The Batch-code processor accepts full Python modules (e.g. "batch.py") that
+# look identical to normal Python files with a few exceptions that allows them
+# to be executed in blocks. This way of working assures a sequential execution
 # of the file and allows for features like stepping from block to block
 # (without executing those coming before), as well as automatic deletion
 # of created objects etc. You can however also run a batch-code python file
@@ -15,13 +15,13 @@
 # #HEADER - this denotes commands global to the entire file, such as
 #           import statements and global variables. They will
 #           automatically be made available for each block.  Observe
-#           that changes to these variables made in one block is not
+#           that changes to these variables made in one block are not
 #           preserved between blocks!)
 # #CODE (infotext) [objname, objname, ...] - This designates a code block that
 #            will be executed like a stand-alone piece of code together with
 #            any #HEADER defined.
-#            infotext is a describing text about what goes in in this block.
-#            It will be shown by the batchprocessing command.
+#            infotext is a describing text about what goes on in this block.
+#            It will be shown by the batch-processing command.
 #            <objname>s mark the (variable-)names of objects created in
 #            the code, and which may be auto-deleted by the processor if
 #            desired (such as when debugging the script). E.g., if the code
@@ -54,7 +54,7 @@ limbo = search_object('Limbo')[0]
 
 #CODE (create red button)
 
-# This is the first code block. Within each block, python
+# This is the first code block. Within each block, Python
 # code works as normal. Note how we make use if imports and
 # 'limbo' defined in the #HEADER block. This block's header
 # offers no information about red_button variable, so it
@@ -75,7 +75,7 @@ caller.msg("A %s was created." % red_button.key)
 # again (so as to avoid duplicate objects when testing the script many
 # times).
 
-# the python variables we assign to must match the ones given in the
+# the Python variables we assign to must match the ones given in the
 # header for the system to be able to delete them afterwards during a
 # debugging run.
 table = create_object(DefaultObject, key="Table", location=limbo)
