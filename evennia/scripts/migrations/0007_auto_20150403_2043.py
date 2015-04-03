6644effef8ff2a75f7361c127f71c1b1a121a009
Griatch <griatch@gmail.com>
2015-04-03 20:49:18 +0200

    Added migrations for the 1.8+ change

---
+++ evennia/scripts/migrations/0007_auto_20150403_2043.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('scripts', '0006_auto_20150310_2249'),
+    ]
+
+    operations = [
+        migrations.DeleteModel(
+            name='DefaultScript',
+        ),
+        migrations.DeleteModel(
+            name='DoNothing',
+        ),
+        migrations.DeleteModel(
+            name='ScriptBase',
+        ),
+        migrations.DeleteModel(
+            name='Store',
+        ),
+    ]
