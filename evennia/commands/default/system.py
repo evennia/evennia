33154d4d609b8c8004964dbf610b1b44512ba30e
Griatch <griatch@gmail.com>
2015-05-14 20:18:39 +0200

    Reworked server reboot code to make the cold reset function work as documented and not disconnect any sessions. Resolves #742.

--- evennia/commands/default/system.py
+++ evennia/commands/default/system.py
@@ -65,10 +65,18 @@ class CmdReset(MuxCommand):
     Usage:
       @reset
 
-    A cold reboot. This works like a mixture of @reload and @shutdown,
-    - all shutdown hooks will be called and non-persistent scripts will
-    be purged. But the Portal will not be affected and the server will
-    automatically restart again.
+    Notes:
+      For normal updating you are recommended to use @reload rather
+      than this command. Use @shutdown for a complete stop of
+      everything.
+
+    This emulates a cold reboot of the Server component of Evennia.
+    The difference to @shutdown is that the Server will auto-reboot
+    and that it does not affect the Portal, so no users will be
+    disconnected. Contrary to @reload however, all shutdown hooks will
+    be called and any non-database saved scripts, ndb-attributes,
+    cmdsets etc will be wiped.
+
     """
     key = "@reset"
     aliases = ['@reboot']
