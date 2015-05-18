0c076b3966a9ed35a04821d0d0c04aa68f5f7648
Peter Finlayson <frnknstn@iafrica.com>
2015-05-18 17:46:18 +0200

    removed references to closed issue #241

--- evennia/game_template/server/conf/settings.py
+++ evennia/game_template/server/conf/settings.py
@@ -40,7 +40,7 @@ HTTP_LOG_FILE = os.path.join(LOG_DIR, 'http_requests.log')
 # ENGINE - path to the the database backend. Possible choices are:
 #            'django.db.backends.sqlite3', (default)
 #            'django.db.backends.mysql',
-#            'django.db.backends.postgresql_psycopg2' (see Issue 241),
+#            'django.db.backends.postgresql_psycopg2',
 #            'django.db.backends.oracle' (untested).
 # NAME - database name, or path to the db file for sqlite3
 # USER - db admin (unused in sqlite3)
--- evennia/settings_default.py
+++ evennia/settings_default.py
@@ -201,7 +201,7 @@ COMMAND_RATE_WARNING ="You entered commands too fast. Wait a moment and try agai
 # ENGINE - path to the the database backend. Possible choices are:
 #            'django.db.backends.sqlite3', (default)
 #            'django.db.backends.mysql',
-#            'django.db.backends.'postgresql_psycopg2' (see Issue 241),
+#            'django.db.backends.'postgresql_psycopg2',
 #            'django.db.backends.oracle' (untested).
 # NAME - database name, or path to the db file for sqlite3
 # USER - db admin (unused in sqlite3)
