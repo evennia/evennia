98d535ebbab64cce404c8dc75709290d17974dd4
Sidharth Kapur <skapur@twitter.com>
2015-07-22 11:42:39 -0700

    Strip language definition

--- recommonmark/parser.py
+++ recommonmark/parser.py
@@ -62,7 +62,7 @@ class CommonMarkParser(parsers.Parser):
             self.verbatim(block.string_content)
         elif (block.t == "FencedCode"):
             if len(block.strings) and len(block.strings[0]):
-                self.code(block.strings[0], block.string_content)
+                self.code(block.strings[0].strip(), block.string_content)
             else:
                 self.verbatim(block.string_content)
         elif (block.t == "ReferenceDef"):
