7f11256fc8fdc51883a921089bacb7c5b40e4030
Griatch <griatch@gmail.com>
2015-10-01 10:30:17 +0200

    Fixed a lingering bug where the call signature of a call to at_search_result from the tutorial_world was not updated properly. Resolves #809.

--- evennia/contrib/tutorial_world/rooms.py
+++ evennia/contrib/tutorial_world/rooms.py
@@ -164,7 +164,7 @@ class CmdTutorialLook(default_cmds.CmdLook):
                 else:
                     # no detail found, delegate our result to the normal
                     # error message handler.
-                    _SEARCH_AT_RESULT(caller, args, looking_at_obj)
+                    _SEARCH_AT_RESULT(None, caller, args, looking_at_obj)
                     return
             else:
                 # we found a match, extract it from the list and carry on
