c811d37a8ab1e72d272bc3abd6c4de550c56fc51
Doug Miller <d.orkim@gmail.com>
2016-01-09 05:19:55 +0000

    fixed some help text for @ban command

--- evennia/commands/default/admin.py
+++ evennia/commands/default/admin.py
@@ -125,16 +125,15 @@ class CmdBan(MuxCommand):
 
     Without any arguments, shows numbered list of active bans.
 
-    This command bans a user from accessing the game. Supply an
-    optional reason to be able to later remember why the ban was put in
-    place
+    This command bans a user from accessing the game. Supply an optional
+    reason to be able to later remember why the ban was put in place.
 
-    It is often to
-    prefer over deleting a player with @delplayer. If banned by name,
-    that player account can no longer be logged into.
+    It is often preferable to ban a player from the server than to
+    delete a player with @delplayer. If banned by name, that player
+    account can no longer be logged into.
 
-    IP (Internet Protocol) address banning allows to block all access
-    from a specific address or subnet. Use the asterisk (*) as a
+    IP (Internet Protocol) address banning allows blocking all access
+    from a specific address or subnet. Use an asterisk (*) as a
     wildcard.
 
     Examples:
@@ -143,12 +142,11 @@ class CmdBan(MuxCommand):
       @ban/ip 134.233.2.*     - ban all in a subnet
       @ban/ip 134.233.*.*     - even wider ban
 
-    A single IP filter is easy to circumvent by changing the computer
-    (also, some ISPs assign only temporary IPs to their users in the
-    first placer. Widening the IP block filter with wildcards might be
-    tempting, but remember that blocking too much may accidentally
-    also block innocent users connecting from the same country and
-    region.
+    A single IP filter can be easy to circumvent by changing computers
+    or requesting a new IP address. Setting a wide IP block filter with
+    wildcards might be tempting, but remember that it may also
+    accidentally block innocent users connecting from the same country
+    or region.
 
     """
     key = "@ban"
