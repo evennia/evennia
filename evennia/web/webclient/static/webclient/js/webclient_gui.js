989a33fc2ae2a4ca1df0c118fb227ef9b3f7f219
Griatch <griatch@gmail.com>
2016-02-10 22:59:01 +0100

    Making a separate webclient_gui file to handle the graphical interface.

---
+++ evennia/web/webclient/static/webclient/js/webclient_gui.js
@@ -0,0 +1,28 @@
+/*
+ *
+ * Evennia Webclient GUI component
+ *
+ * This is used in conjunction with the main evennia.js library, which
+ * handles all the communication with the Server.
+ *
+ * The job of this code is to create listeners to subscribe to evennia
+ * messages, via Evennia.emitter.on(cmdname, listener) and to handle
+ * input from the user and send it to 
+ * Evennia.msg(cmdname, args, kwargs, [callback]).
+ *
+ */
+
+// 
+// GUI Helpers
+//
+
+
+
+// 
+// Listeners 
+//
+
+
+// 
+// Senders 
+//
