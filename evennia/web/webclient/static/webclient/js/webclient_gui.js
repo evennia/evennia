f2213c8a6b4ba33071d920ff1c89603392afbc43
Griatch <griatch@gmail.com>
2016-02-11 21:55:44 +0100

    Made a rudimentary gui for the webclient. Some data is going through to webclient, but currently html conversion is not working.

--- evennia/web/webclient/static/webclient/js/webclient_gui.js
+++ evennia/web/webclient/static/webclient/js/webclient_gui.js
@@ -13,16 +13,152 @@
  */
 
 // 
-// GUI Helpers
+// GUI Elements
 //
 
+
+//
+// Manage history for input line
+//
+var inputlog = function() {
+    var history_max = 21;
+    var history = new Array();
+    var history_pos = 0;
+
+    history[0] = ''; // the very latest input is empty for new entry.
+
+    function history_back() {
+        // step backwards in history stack 
+        history_pos = Math.min(++history_pos, history.length - 1);
+        return history[history.length - 1 - history_pos]; 
+    }
+    function history_fwd() {
+        // step forwards in history stack
+        history_pos = Math.max(--history_pos, 0);
+        return history[history.length -1 - history_pos];
+    }
+    function history_add(input) {
+        // add a new entry to history, don't repeat latest
+        if (input != history[history.length-1]) {
+            if (history.length >= history_max) {
+                history.shift(); // kill oldest entry
+            }
+            history[history.length-1] = input;
+            history[history.length] = '';
+        }
+    }
+    return {back: history_back, 
+            fwd: history_fwd,
+            add: history_add}
+};
+
+$.fn.appendCaret = function() {
+    /* jQuery extension that will forward the caret to the end of the input, and
+       won't harm other elements (although calling this on multiple inputs might
+       not have the expected consequences).
+
+       Thanks to
+       http://stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area
+       for the good starting point.  */
+    return this.each(function() {
+        var range,
+            // Index at where to place the caret.
+            end,
+            self = this;
+
+        if (self.setSelectionRange) {
+            // other browsers
+            end = self.value.length;
+            self.focus();
+            // NOTE: Need to delay the caret movement until after the callstack.
+            setTimeout(function() {
+                self.setSelectionRange(end, end);
+            }, 0);
+        }
+        else if (self.createTextRange) {
+            // IE
+            end = self.value.length - 1;
+            range = self.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', end);
+            range.moveStart('character', end);
+            // NOTE: I haven't tested to see if IE has the same problem as
+            // W3C browsers seem to have in this context (needing to fire
+            // select after callstack).
+            range.select();
+        }
+    });
+};
+
+
+// GUI Event Handlers 
+
+$(document).keydown( function(event) {
+    // catch all keyboard input, handle special chars
+    var code = event.which;
+    inputfield = $("#inputfield");
+    inputfield.focus();
+
+    if (code === 13) { // Enter key sends text
+        outtext = inputfield.val()
+        inputlog.add(outtext)
+        Evennia.msg("text", [outtext], {});
+        event.prevetDefault()
+    }
+    else if (code === 38) { // Arrow up
+        inputfield.val(inputlog.back()).appendCaret();
+    }
+    else if (code === 40) { // Arrow down
+        inputfield.val(inputlog.fwd()).appendCaret();
+    }
+
+});
+
+// client size setter
+
+function set_window_size() {
+    var winh = $(document).height();
+    var formh = $('#inputform').outerHeight(true);
+    $("#messagewindow").css({'height': winh - formh - 1});
+}
+
+// Event - called when window resizes
+$(window).resize(set_window_size);
 
 
 // 
 // Listeners 
 //
 
+function doText(args, kwargs) {
+    // append message to previous ones
+    $("#messagewindow").append(
+            "<div class='msg out>" + args[0] + "</div>");
+    // scroll message window to bottom
+    $("#messagewindow").animate({scrollTop: $('#messageindow')[0].scrollHeight});
+}
 
-// 
-// Senders 
-//
+function doPrompt(args, kwargs) {
+    // show prompt
+    $('prompt').replaceWith(
+           "<div id='prompt' class='msg out'>" + args[0] + "</div>");
+}
+
+
+$(document).ready(function() {
+    // a small timeout to stop 'loading' indicator in Chrome
+    Evennia.init() 
+    // register listeners
+    Evennia.emitter.on("text", doText);
+    Evennia.emitter.on("prompt", doPrompt);
+    set_window_size();    
+    
+    // set an idle timer to avoid proxy servers to time out on us (every 3 minutes)
+    setInterval(function() {
+        log('Idle tick.');
+        Evennia.msg("text", ["idle"], {});
+    }, 
+    60000*3
+    );
+});
+
