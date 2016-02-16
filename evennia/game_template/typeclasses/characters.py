ba1f10e88fa0c2087fadbcc01e08645e2385fb6f
Griatch <griatch@gmail.com>
2016-02-16 10:12:57 +0100

    Made a more updated doc string.

--- evennia/game_template/typeclasses/characters.py
+++ evennia/game_template/typeclasses/characters.py
@@ -11,21 +11,22 @@ from evennia import DefaultCharacter
 
 class Character(DefaultCharacter):
     """
-    The Character defaults to implementing some of its hook methods with the
-    following standard functionality:
+    The Character defaults to reimplementing some of base Object's hook methods with the
+    following functionality:
 
     at_basetype_setup - always assigns the DefaultCmdSet to this object type
                     (important!)sets locks so character cannot be picked up
                     and its commands only be called by itself, not anyone else.
-                    (to change things, use at_object_creation() instead)
-    at_after_move - launches the "look" command
-    at_post_puppet(player) -  when Player disconnects from the Character, we
-                    store the current location, so the "unconnected" character
-                    object does not need to stay on grid but can be given a
-                    None-location while offline.
-    at_pre_puppet - just before Player re-connects, retrieves the character's
-                    old location and puts it back on the grid with a "charname
-                    has connected" message echoed to the room
+                    (to change things, use at_object_creation() instead).
+    at_after_move - Launches the "look" command after every move.
+    at_post_unpuppet(player) -  when Player disconnects from the Character, we
+                    store the current location in the pre_logout_location Attribute and
+                    move it to a None-location so the "unpuppeted" character
+                    object does not need to stay on grid. Echoes "Player has disconnected" 
+                    to the room.
+    at_pre_puppet - Just before Player re-connects, retrieves the character's
+                    pre_logout_location Attribute and move it back on the grid.
+    at_post_puppet - Echoes "PlayerName has entered the game" to the room.
 
     """
     pass
