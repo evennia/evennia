7584516df2f3c89e8a64d2ec8f6207751ac68c2f
Matthias Geier <Matthias.Geier@gmail.com>
2016-04-24 12:32:25 +0200

    Turn package into a Sphinx extension

--- recommonmark/__init__.py
+++ recommonmark/__init__.py
@@ -1,1 +1,8 @@
 __version__ = '0.4.0'
+
+
+def setup(app):
+    """Initialize Sphinx extension."""
+    from .parser import CommonMarkParser
+    app.add_source_parser('.md', CommonMarkParser)  # needs Sphinx >= 1.4
+    return {'version': __version__, 'parallel_read_safe': True}
