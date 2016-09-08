e0e8e9453a116026fa19a4fb5f4b99e31305b971
Griatch <griatch@gmail.com>
2016-09-08 20:49:25 +0200

    Fix edge case with the who command and players with puppets at None locations. Resolves #1050.

--- evennia/commands/default/player.py
+++ evennia/commands/default/player.py
@@ -422,7 +422,7 @@ class CmdWho(COMMAND_DEFAULT_CLASS):
                 delta_conn = time.time() - session.conn_time
                 player = session.get_player()
                 puppet = session.get_puppet()
-                location = puppet.location.key if puppet else "None"
+                location = puppet.location.key if puppet and puppet.location else "None"
                 table.add_row([utils.crop(player.name, width=25),
                                utils.time_format(delta_conn, 0),
                                utils.time_format(delta_cmd, 1),
