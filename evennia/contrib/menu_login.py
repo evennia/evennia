8561e3de23ff5e756937bf360b35e85ae67fb822
Griatch <griatch@gmail.com>
2016-10-18 23:32:32 +0200

    Cleaned up the menu_system.py contrib docstring a little.

--- evennia/contrib/menu_login.py
+++ evennia/contrib/menu_login.py
@@ -3,36 +3,32 @@ A login menu using EvMenu.
 
 Contribution - Vincent-lg 2016
 
-This module defines a simple login system, similar to the one
-defined in 'menu_login.py".  This present menu system, however,
-uses EvMenu (hence the name).  This module contains the
-functions (nodes) of the menu, with the CmdSet and
-UnloggedCommand called when a user logs in.  In other words,
-instead of using the 'connect' or 'create' commands once on the
-login screen, players have to navigate through a simple menu
-asking them to enter their username (then password), or to type
-'new' to create one.  You may want to update your login screen
-if you use this system.
+This module contains the functions (nodes) of the EvMenu, with the
+CmdSet and UnloggedCommand called when a user logs in.  In other
+words, instead of using the 'connect' or 'create' commands once on the
+login screen, players navigates through a simple menu asking them to
+enter their username followed by password, or to type 'new' to create
+a new one. You will need to change update your login screen if you use
+this system.
 
 In order to install, to your settings file, add/edit the line:
 
 CMDSET_UNLOGGEDIN = "contrib.menu_login.UnloggedinCmdSet"
 
-When you'll reload the server, new sessions will connect to the
-new login system, where they will be able to:
+When you'll reload the server, new sessions will connect to the new
+login system, where they will be able to:
 
 * Enter their username, assuming they have an existing player.
 * Enter 'NEW' to create a new player.
 
-The top-level functions in this file are menu nodes (as
-described in EvMenu).  Each one of these functions is
-responsible for prompting the user with a specific information
-(username, password and so on).  At the bottom of the file are
-defined the CmdSet for Unlogging users, which adds a new command
-(defined below) that is called just after a new session has been
-created, in order to create the menu.  See the specific
-documentation on functions (nodes) to see what each one should
-do.
+The top-level functions in this file are menu nodes (as described in
+evennia.utils.evmenu.py). Each one of these functions is responsible
+for prompting the user for a specific piece of information (username,
+password and so on). At the bottom of the file is defined the CmdSet
+for Unlogging users. This adds a new command that is called just after
+a new session has been created, in order to create the menu.  See the
+specific documentation on functions (nodes) to see what each one
+should do.
 
 """
 
