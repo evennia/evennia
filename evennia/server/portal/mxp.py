8bf43797319a85db6728432ebe54c074e8600ad0
Griatch <griatch@gmail.com>
2016-11-29 11:41:39 +0100

    Removed the html quote replacement for Telnet handling; this deals with the remainders of #1058 and was tested both with Mudlet and MUSHclient to work correctly.

--- evennia/server/portal/mxp.py
+++ evennia/server/portal/mxp.py
@@ -38,7 +38,6 @@ def mxp_parse(text):
 
     """
     text = text.replace("&", "&amp;") \
-               .replace('"', "&quot;") \
                .replace("<", "&lt;") \
                .replace(">", "&gt;")
 
