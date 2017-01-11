9e505b9b72ae5390ce1ecf61b21b4f3447125523
Griatch <griatch@gmail.com>
2017-01-11 21:21:56 +0100

    Clean up spurious whitespace.

--- evennia/players/players.py
+++ evennia/players/players.py
@@ -728,7 +728,7 @@ class DefaultPlayer(with_metaclass(TypeclassBase, PlayerDB)):
         # inform the client that we logged in through an OOB message
         if session:
             session.msg(logged_in={})
-            
+
         self._send_to_connect_channel("|G%s connected|n" % self.key)
         if _MULTISESSION_MODE == 0:
             # in this mode we should have only one character available. We
