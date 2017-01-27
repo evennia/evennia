fbf8ef4075f3088f48c9384e11056fbfc26f96da
Simon Vermeersch <simonvermeersch@gmail.com>
2017-01-27 22:17:29 +0100

    Added a wilderness area contrib

--- evennia/contrib/tests.py
+++ evennia/contrib/tests.py
@@ -298,4 +298,122 @@ class TestBarter(CommandTest):
         self.call(barter.CmdFinish(), ": Ending.", "You say, \"Ending.\"\n  [You aborted trade. No deal was made.]")
 
 
+from evennia.contrib import wilderness
+from evennia import DefaultCharacter
 
+
+class TestWilderness(EvenniaTest):
+
+    def setUp(self):
+        super(TestWilderness, self).setUp()
+        self.char1 = create_object(DefaultCharacter, key="char1")
+        self.char2 = create_object(DefaultCharacter, key="char2")
+
+    def get_wilderness_script(self, name="default"):
+        w = wilderness.WildernessScript.objects.get("default")
+        return w
+
+    def test_create_wilderness_default_name(self):
+        wilderness.create_wilderness()
+        w = self.get_wilderness_script()
+        self.assertIsNotNone(w)
+
+    def test_create_wilderness_custom_name(self):
+        name = "customname"
+        wilderness.create_wilderness(name)
+        w = self.get_wilderness_script(name)
+        self.assertIsNotNone(w)
+
+    def test_enter_wilderness(self):
+        wilderness.create_wilderness()
+        wilderness.enter_wilderness(self.char1)
+        self.assertIsInstance(self.char1.location, wilderness.WildernessRoom)
+        w = self.get_wilderness_script()
+        self.assertEquals(w.db.itemlocations[self.char1], (0, 0))
+
+    def test_enter_wilderness_custom_location(self):
+        wilderness.create_wilderness()
+        wilderness.enter_wilderness(self.char1, location=(1, 2))
+        self.assertIsInstance(self.char1.location, wilderness.WildernessRoom)
+        w = self.get_wilderness_script()
+        self.assertEquals(w.db.itemlocations[self.char1], (1, 2))
+
+    def test_enter_wilderness_custom_name(self):
+        name = "customnname"
+        wilderness.create_wilderness(name)
+        wilderness.enter_wilderness(self.char1, name=name)
+        self.assertIsInstance(self.char1.location, wilderness.WildernessRoom)
+
+    def test_wilderness_correct_exits(self):
+        wilderness.create_wilderness()
+        wilderness.enter_wilderness(self.char1)
+
+        # By default we enter at a corner (0, 0), so only a few exits should
+        # be visible / traversable
+        exits = [i for i in self.char1.location.contents
+                 if i.destination and (
+                  i.access(self.char1, "view") or
+                  i.access(self.char1, "traverse"))]
+
+        self.assertEquals(len(exits), 3)
+        exitsok = ["north", "northeast", "east"]
+        for exit in exitsok:
+            self.assertTrue(any([e for e in exits if e.key == exit]))
+
+        # If we move to another location not on an edge, then all directions
+        # should be visible / traversable
+        wilderness.enter_wilderness(self.char1, location=(1, 1))
+        exits = [i for i in self.char1.location.contents
+                 if i.destination and (
+                  i.access(self.char1, "view") or
+                  i.access(self.char1, "traverse"))]
+        self.assertEquals(len(exits), 8)
+        exitsok = ["north", "northeast", "east", "southeast", "south",
+                   "southwest", "west", "northwest"]
+        for exit in exitsok:
+            self.assertTrue(any([e for e in exits if e.key == exit]))
+
+    def test_room_creation(self):
+        # Pretend that both char1 and char2 are connected...
+        self.char1.sessions.add(1)
+        self.char2.sessions.add(1)
+        self.assertTrue(self.char1.has_player)
+        self.assertTrue(self.char2.has_player)
+
+        wilderness.create_wilderness()
+        w = self.get_wilderness_script()
+
+        # We should have no unused room after moving the first player in.
+        self.assertEquals(len(w.db.unused_rooms), 0)
+        w.move_obj(self.char1, (0, 0))
+        self.assertEquals(len(w.db.unused_rooms), 0)
+
+        # And also no unused room after moving the second one in.
+        w.move_obj(self.char2, (1, 1))
+        self.assertEquals(len(w.db.unused_rooms), 0)
+
+        # But if char2 moves into char1's room, we should have one unused room
+        # Which should be char2's old room that got created.
+        w.move_obj(self.char2, (0, 0))
+        self.assertEquals(len(w.db.unused_rooms), 1)
+        self.assertEquals(self.char1.location, self.char2.location)
+
+        # And if char2 moves back out, that unused room should be put back to
+        # use again.
+        w.move_obj(self.char2, (1, 1))
+        self.assertNotEquals(self.char1.location, self.char2.location)
+        self.assertEquals(len(w.db.unused_rooms), 0)
+
+    def test_get_new_location(self):
+        loc = (1, 1)
+        directions = {"north": (1, 2),
+                      "northeast": (2, 2),
+                      "east": (2, 1),
+                      "southeast": (2, 0),
+                      "south": (1, 0),
+                      "southwest": (0, 0),
+                      "west": (0, 1),
+                      "northwest": (0, 2)}
+        for direction, correct_loc in directions.iteritems():
+            new_loc = wilderness.get_new_location(loc, direction)
+            self.assertEquals(new_loc, correct_loc, direction)
