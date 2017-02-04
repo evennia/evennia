1a3b9e251544f91b596c49748965a57ee3bf004d
Tehom <surly.mime@gmail.com>
2017-02-04 12:58:43 -0500

    removed app_label, no longer necessary since removed custom AppConfig class from initial attempt.

--- evennia/typeclasses/attributes.py
+++ evennia/typeclasses/attributes.py
@@ -99,7 +99,6 @@ class Attribute(SharedMemoryModel):
     class Meta(object):
         "Define Django meta options"
         verbose_name = "Evennia Attribute"
-        app_label = 'typeclasses'
 
     # read-only wrappers
     key = property(lambda self: self.db_key)
