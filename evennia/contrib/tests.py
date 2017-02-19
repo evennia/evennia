695cc4d2188ba4ed68211b725a2b08c5db0e823e
Griatch <griatch@gmail.com>
2017-02-20 00:11:08 +0100

    Add basic unittests for the rest of the tutorial_world rooms. This concludes #1105.

--- evennia/contrib/tests.py
+++ evennia/contrib/tests.py
@@ -724,3 +724,22 @@ class TestTutorialWorldRooms(CommandTest):
     def test_introroom(self):
         room = create_object(tutrooms.IntroRoom, key="introroom")
         room.at_object_receive(self.char1, self.room1)
+    def test_bridgeroom(self):
+        room = create_object(tutrooms.BridgeRoom, key="bridgeroom")
+        room.update_weather()
+        self.char1.move_to(room)
+        self.call(tutrooms.CmdBridgeHelp(), "", "You are trying hard not to fall off the bridge ...", obj=room)
+        self.call(tutrooms.CmdLookBridge(), "", "bridgeroom\nYou are standing very close to the the bridge's western foundation.", obj=room)
+        room.at_object_leave(self.char1, self.room1)
+        tutrooms.TICKER_HANDLER.remove(interval=room.db.interval, callback=room.update_weather, idstring="tutorial")
+        room.delete()
+    def test_darkroom(self):
+        room = create_object(tutrooms.DarkRoom, key="darkroom")
+        self.char1.move_to(room)
+        self.call(tutrooms.CmdDarkHelp(), "", "Can't help you until")
+    def test_teleportroom(self):
+        create_object(tutrooms.TeleportRoom, key="teleportroom")
+    def test_outroroom(self):
+        create_object(tutrooms.OutroRoom, key="outroroom")
+
+
