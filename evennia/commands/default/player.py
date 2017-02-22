ab12965f14ff3c45b43300acace38d2313d7576a
BlauFeuer <phaselinear700series2@gmail.com>
2017-02-22 14:41:29 -0500

    Exclude prettytable conversion

--- evennia/commands/default/player.py
+++ evennia/commands/default/player.py
@@ -23,7 +23,7 @@ from builtins import range
 import time
 from django.conf import settings
 from evennia.server.sessionhandler import SESSIONS
-from evennia.utils import utils, create, search, evtable
+from evennia.utils import utils, create, search, prettytable, evtable
 
 COMMAND_DEFAULT_CLASS = utils.class_from_module(settings.COMMAND_DEFAULT_CLASS)
 
@@ -356,18 +356,18 @@ class CmdSessions(COMMAND_DEFAULT_CLASS):
         """Implement function"""
         player = self.player
         sessions = player.sessions.all()
-        table = evtable.EvTable("|wsessid",
-                                "|wprotocol",
-                                "|whost",
-                                "|wpuppet/character",
-                                "|wlocation")
+        table = prettytable.PrettyTable(["|wsessid",
+                                         "|wprotocol",
+                                         "|whost",
+                                         "|wpuppet/character",
+                                         "|wlocation"])
         for sess in sorted(sessions, key=lambda x: x.sessid):
             char = player.get_puppet(sess)
-            table.add_row(str(sess.sessid), str(sess.protocol_key),
-                          type(sess.address) == tuple and sess.address[0] or sess.address,
-                          char and str(char) or "None",
-                          char and str(char.location) or "N/A")
-            self.msg("|wYour current session(s):|n\n%s" % table)
+            table.add_row([str(sess.sessid), str(sess.protocol_key),
+                           type(sess.address) == tuple and sess.address[0] or sess.address,
+                           char and str(char) or "None",
+                           char and str(char.location) or "N/A"])
+        self.msg("|wYour current session(s):|n\n%s" % table)
 
 
 class CmdWho(COMMAND_DEFAULT_CLASS):
@@ -407,14 +407,14 @@ class CmdWho(COMMAND_DEFAULT_CLASS):
         nplayers = (SESSIONS.player_count())
         if show_session_data:
             # privileged info
-            table = evtable.EvTable("|wPlayer Name",
-                                    "|wOn for",
-                                    "|wIdle",
-                                    "|wPuppeting",
-                                    "|wRoom",
-                                    "|wCmds",
-                                    "|wProtocol",
-                                    "|wHost")
+            table = prettytable.PrettyTable(["|wPlayer Name",
+                                             "|wOn for",
+                                             "|wIdle",
+                                             "|wPuppeting",
+                                             "|wRoom",
+                                             "|wCmds",
+                                             "|wProtocol",
+                                             "|wHost"])
             for session in session_list:
                 if not session.logged_in:
                     continue
@@ -423,26 +423,27 @@ class CmdWho(COMMAND_DEFAULT_CLASS):
                 player = session.get_player()
                 puppet = session.get_puppet()
                 location = puppet.location.key if puppet and puppet.location else "None"
-                table.add_row(utils.crop(player.name, width=25),
-                              utils.time_format(delta_conn, 0),
-                              utils.time_format(delta_cmd, 1),
-                              utils.crop(puppet.key if puppet else "None", width=25),
-                              utils.crop(location, width=25),
-                              session.cmd_total,
-                              session.protocol_key,
-                              isinstance(session.address, tuple) and session.address[0] or session.address)
+                table.add_row([utils.crop(player.name, width=25),
+                               utils.time_format(delta_conn, 0),
+                               utils.time_format(delta_cmd, 1),
+                               utils.crop(puppet.key if puppet else "None", width=25),
+                               utils.crop(location, width=25),
+                               session.cmd_total,
+                               session.protocol_key,
+                               isinstance(session.address, tuple) and session.address[0] or session.address])
         else:
             # unprivileged
-            table = evtable.EvTable("|wPlayer name", "|wOn for", "|wIdle")
+            table = prettytable.PrettyTable(["|wPlayer name", "|wOn for", "|wIdle"])
             for session in session_list:
                 if not session.logged_in:
                     continue
                 delta_cmd = time.time() - session.cmd_last_visible
                 delta_conn = time.time() - session.conn_time
                 player = session.get_player()
-                table.add_row(utils.crop(player.key, width=25),
-                              utils.time_format(delta_conn, 0),
-                              utils.time_format(delta_cmd, 1))
+                table.add_row([utils.crop(player.key, width=25),
+                               utils.time_format(delta_conn, 0),
+                               utils.time_format(delta_cmd, 1)])
+
         is_one = nplayers == 1
         self.msg("|wPlayers:|n\n%s\n%s unique account%s logged in."
                  % (table, "One" if is_one else nplayers, "" if is_one else "s"))
