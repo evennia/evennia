5e43f1370f97ff82f5bfd6fbdb7acbb6b084ee29
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2017-03-13 13:16:46 -0700

    Set @event/edit to be more clever when selecting events

--- evennia/contrib/events/typeclasses.py
+++ evennia/contrib/events/typeclasses.py
@@ -28,8 +28,9 @@ class PatchedExit(object):
             target_location (Object): Where target is going.
 
         """
-        if inherits_from(traversing_object, DefaultCharacter):
-            script = ScriptDB.objects.get(db_key="event_handler")
+        is_character = inherits_from(traversing_object, DefaultCharacter)
+        script = ScriptDB.objects.get(db_key="event_handler")
+        if is_character:
             allow = script.call_event(exit, "can_traverse", traversing_object,
                     exit, exit.location)
             if not allow:
@@ -37,12 +38,33 @@ class PatchedExit(object):
 
         hook(exit, traversing_object, target_location)
 
+        # After traversing
+        if is_character:
+            script.call_event(exit, "traverse", traversing_object,
+                    exit, exit.location, exit.destination)
+
+
 # Default events
 create_event_type(DefaultExit, "can_traverse", ["character", "exit", "room"],
-    """Can the character traverse through this exit?
+    """
+    Can the character traverse through this exit?
     This event is called when a character is about to traverse this
     exit.  You can use the deny() function to deny the character from
     using this exit for the time being.  The 'character' variable
     contains the character who wants to traverse through this exit.
     The 'exit' variable contains the exit, the 'room' variable
-    contains the room in which the character and exit are.""")
+    contains the room in which the character and exit are.
+""")
+create_event_type(DefaultExit, "traverse", ["character", "exit",
+        "origin", "destination"], """
+    After the characer has traversed through this exit.
+    This event is called after a character has traversed through this
+    exit.  Traversing cannot be prevented using 'deny()' at this
+    point.  The character will be in a different room and she will
+    have received the room's description when this event is called.
+    The 'character' variable contains the character who has traversed
+    through this exit.  The 'exit' variable contains the exit, the
+    'origin' variable contains the room in which the character was
+    before traversing, while 'destination' contains the room in which
+    the character now is.
+    """)
