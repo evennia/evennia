a14e11640b9f107ebcf538aa71f6149214c57368
Griatch <griatch@gmail.com>
2017-07-07 22:38:24 +0200

    Correction of the migration sequence both for clean and old databases.

--- evennia/objects/migrations/0001_initial.py
+++ evennia/objects/migrations/0001_initial.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('typeclasses', '0001_initial'),
+        ('typeclasses', '0002_auto_20150109_0913'),
     ]
 
     operations = [
