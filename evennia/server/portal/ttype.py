994e8f6fb0bfa26667842147c23e275ab2ba6b5d
Griatch <griatch@gmail.com>
2017-08-19 23:21:45 +0200

    Fix line ending error caused by autopep8 run

--- evennia/server/portal/ttype.py
+++ evennia/server/portal/ttype.py
@@ -133,8 +133,8 @@ class Ttype(object):
             term = option
             tupper = term.upper()
             # identify xterm256 based on flag
-            xterm256 = (tupper.endswith("-256COLOR")         # Apple Terminal, old Tintin or
-                        tupper.endswith("XTERM") and      # old Tintin, Putty
+            xterm256 = (tupper.endswith("-256COLOR") or        # Apple Terminal, old Tintin
+                        tupper.endswith("XTERM") and           # old Tintin, Putty
                         not tupper.endswith("-COLOR"))
             if xterm256:
                 self.protocol.protocol_flags['ANSI'] = True
