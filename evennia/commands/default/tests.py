07250ee5aeb2ccfbc3b76b176088e4e986cbbb6e
BlauFeuer <phaselinear700series2@gmail.com>
2017-09-07 14:37:15 -0400

    Update test calling CmdWhisper (format change)

--- evennia/commands/default/tests.py
+++ evennia/commands/default/tests.py
@@ -133,7 +133,7 @@ class TestGeneral(CommandTest):
         self.call(general.CmdSay(), "Testing", "You say, \"Testing\"")
 
     def test_whisper(self):
-        self.call(general.CmdWhisper(), "Obj = Testing", "You whisper to Obj, \"Testing\"", caller=self.char2)
+        self.call(general.CmdWhisper(), "Obj = Testing", "You whisper \"Testing\" to Obj.", caller=self.char2)
 
     def test_access(self):
         self.call(general.CmdAccess(), "", "Permission Hierarchy (climbing):")
