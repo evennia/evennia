0ba913332a2a1049a9d71322119db8dc7fb9c5b5
Griatch <griatch@gmail.com>
2017-09-30 10:12:46 +0200

    [fix] The `@desc` command was erroneously still named `@setdesc`, this was not the intention. Relates to #1450.

--- evennia/commands/default/building.py
+++ evennia/commands/default/building.py
@@ -543,7 +543,7 @@ class CmdDesc(COMMAND_DEFAULT_CLASS):
     describe an object or the current room.
 
     Usage:
-      @setdesc [<obj> =] <description>
+      @desc [<obj> =] <description>
 
     Switches:
       edit - Open up a line editor for more advanced editing.
@@ -551,7 +551,7 @@ class CmdDesc(COMMAND_DEFAULT_CLASS):
     Sets the "desc" attribute on an object. If an object is not given,
     describe the current room.
     """
-    key = "@setdesc"
+    key = "@desc"
     aliases = "@describe"
     locks = "cmd:perm(desc) or perm(Builder)"
     help_category = "Building"
@@ -706,10 +706,10 @@ class CmdDestroy(COMMAND_DEFAULT_CLASS):
             else:
                 confirm += ", ".join(["#{}".format(obj.id) for obj in objs])
             confirm += " [yes]/no?" if self.default_confirm == 'yes' else " yes/[no]"
-            answer = yield(confirm)
-            answer = self.default_confirm if answer == '' else answer
+            answer = ""
             while answer.strip().lower() not in ("y", "yes", "n", "no"):
                 answer = yield(confirm)
+                answer = self.default_confirm if answer == '' else answer
 
             if answer.strip().lower() in ("n", "no"):
                 caller.msg("Cancelled: no object was destroyed.")
