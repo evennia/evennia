3d2e7a7efd38311c79add2ec9977e782670ccaf8
vonzimr <vonzimr@gmail.com>
2017-10-11 19:53:48 -0700

    remove blank tests

--- evennia/server/portal/tests.py
+++ evennia/server/portal/tests.py
@@ -33,49 +33,49 @@ class TestIRC(TestCase):
         s_irc = "\x02thisisatest"
         s_eve = r'|hthisisatest'
         self.assertEqual(irc.parse_ansi_to_irc(s_eve), s_irc)
-        self.assertEqual(s_eve, irc.prase_irc_to_ansi(s_irc))
-    
+        self.assertEqual(s_eve, irc.parse_irc_to_ansi(s_irc))
+
     def test_italic(self):
         s_irc = "\x02thisisatest"
         s_eve = r'|hthisisatest'
         self.assertEqual(irc.parse_ansi_to_irc(s_eve), s_irc)
 
     def test_colors(self):
-        color_map = (("\0030",  r'|w'), 
-                     ("\0031",  r'|X'), 
-                     ("\0032",  r'|B'), 
-                     ("\0033", r'|G'),
-                     ("\0034", r'|r'),
-                     ("\0035", r'|R'),
-                     ("\0036",  r'|M'),
-                     ("\0037", r'|Y'),
-                     ("\0038",  r'|y'),
-                     ("\0039",  r'|g'),
-                     ("\00310",  r'|C'),
-                     ("\00311",  r'|c'),
-                     ("\00312",  r'|b'),
-                     ("\00313",  r'|m'),
-                     ("\00314", r'|x'),
-                     ("\00315", r'|W'))
- 
+        color_map = (("\0030",     r'|w'),
+                     ("\0031",     r'|X'),
+                     ("\0032",     r'|B'),
+                     ("\0033",     r'|G'),
+                     ("\0034",     r'|r'),
+                     ("\0035",     r'|R'),
+                     ("\0036",     r'|M'),
+                     ("\0037",     r'|Y'),
+                     ("\0038",     r'|y'),
+                     ("\0039",     r'|g'),
+                     ("\00310",    r'|C'),
+                     ("\00311",    r'|c'),
+                     ("\00312",    r'|b'),
+                     ("\00313",    r'|m'),
+                     ("\00314",    r'|x'),
+                     ("\00315",    r'|W'),
+                     ("\00399,5",  r'|[r'),
+                     ("\00399,3",  r'|[g'),
+                     ("\00399,7",  r'|[y'),
+                     ("\00399,2",  r'|[b'),
+                     ("\00399,6",  r'|[m'),
+                     ("\00399,10", r'|[c'),
+                     ("\00399,15", r'|[w'),
+                     ("\00399,1",  r'|[x'))
 
         for m in color_map:
             self.assertEqual(irc.parse_irc_to_ansi(m[0]), m[1])
             self.assertEqual(m[0], irc.parse_ansi_to_irc(m[1]))
 
-    
-    def test_bold(self):
-        s_irc = "\002thisisatest"
-        s_eve = r'|hthisisatest'
-        self.assertEqual(irc.parse_ansi_to_irc(s_eve), s_irc)
-        self.assertEqual(s_eve, irc.parse_irc_to_ansi(s_irc))
-
-    def test_underlined(self):
-        pass
-
     def test_identity(self):
         """
         Test that the composition of the function and
-        its inverse gives the correct string
+        its inverse gives the correct string.
         """
-        pass
+
+        s = r'|wthis|Xis|gis|Ma|C|complex|*string'
+
+        self.assertEqual(irc.parse_irc_to_ansi(irc.parse_ansi_to_irc(s)), s)
