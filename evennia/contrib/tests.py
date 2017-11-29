8cd33261fa93759612f54623febc011060bd40f7
BattleJenkins <Tim@battlestudio.com>
2017-11-29 15:29:49 -0800

    TickerHandler stuff, more

--- evennia/contrib/tests.py
+++ evennia/contrib/tests.py
@@ -1307,7 +1307,7 @@ class TestTurnBattleFunc(EvenniaTest):
         turnhandler.stop()
         # Now time to test item stuff.
         user = create_object(tb_items.TBItemsCharacter, key="User")
-        user.TICKER_HANDLER.remove(interval=30, callback=user.at_update)
+        tb_items.tickerhandler.remove(interval=30, callback=user.at_update)
         testroom = create_object(DefaultRoom, key="Test Room")
         user.location = testroom
         test_healpotion = create_object(key="healing potion")
