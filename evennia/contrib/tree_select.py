cc398f985117f6f8d2f81429b43c77b539444588
Griatch <griatch@gmail.com>
2017-11-29 19:32:50 +0100

    Remove some spurious spaces

--- evennia/contrib/tree_select.py
+++ evennia/contrib/tree_select.py
@@ -28,14 +28,14 @@ on a player:
 The player will be presented with an EvMenu, like so:
 
     ___________________________
-    
+
     Make your selection:
     ___________________________
-    
-    Foo  
-    Bar  
-    Baz  
-    Qux  
+
+    Foo
+    Bar
+    Baz
+    Qux
 
 Making a selection will pass the selection's key to the specified callback as a
 string along with the caller, as well as the index of the selection (the line number
@@ -62,7 +62,7 @@ For example, let's add some more options to our menu, turning 'Bar' into a categ
     --When to walk away
     Baz
     Qux'''
-    
+
 Now when we call the menu, we can see that 'Bar' has become a category instead of a
 selectable option.
 
@@ -71,34 +71,34 @@ selectable option.
     Make your selection:
     _______________________________
 
-    Foo      
-    Bar [+]  
-    Baz      
-    Qux      
-    
+    Foo
+    Bar [+]
+    Baz
+    Qux
+
 Note the [+] next to 'Bar'. If we select 'Bar', it'll show us the option listed under it.
 
     ________________________________________________________________
 
     Bar
     ________________________________________________________________
-    
-    You've got to know [+]                    
-    << Go Back: Return to the previous menu.  
-    
+
+    You've got to know [+]
+    << Go Back: Return to the previous menu.
+
 Just the one option, which is a category itself, and the option to go back, which will
 take us back to the previous menu. Let's select 'You've got to know'.
 
     ________________________________________________________________
-    
+
     You've got to know
     ________________________________________________________________
-    
-    When to hold em                           
-    When to fold em                           
-    When to walk away                         
+
+    When to hold em
+    When to fold em
+    When to walk away
     << Go Back: Return to the previous menu.
-    
+
 Now we see the three options listed under it, too. We can select one of them or use 'Go
 Back' to return to the 'Bar' menu we were just at before. It's very simple to make a
 branching tree of selections!
@@ -115,24 +115,24 @@ description to 'Baz' in our menu:
     --When to walk away
     Baz: Look at this one: the best option.
     Qux'''
-    
+
 Now we see that the Baz option has a description attached that's separate from its key:
 
     _______________________________________________________________
 
     Make your selection:
     _______________________________________________________________
-    
-    Foo                                      
-    Bar [+]                                  
-    Baz: Look at this one: the best option.  
-    Qux                                      
+
+    Foo
+    Bar [+]
+    Baz: Look at this one: the best option.
+    Qux
 
 Once the player makes a selection - let's say, 'Foo' - the menu will terminate and call
 your specified callback with the selection, like so:
 
     callback(caller, TEST_MENU, 0, "Foo")
-    
+
 The index of the selection is given along with a string containing the selection's key.
 That way, if you have two selections in the menu with the same key, you can still
 differentiate between them.
@@ -167,7 +167,7 @@ def init_tree_selection(treestr, caller, callback,
                         start_text="Make your selection:"):
     """
     Prompts a player to select an option from a menu tree given as a multi-line string.
-    
+
     Args:
         treestr (str): Multi-lne string representing menu options
         caller (obj): Player to initialize the menu for
@@ -176,33 +176,33 @@ def init_tree_selection(treestr, caller, callback,
             treestr (str): Menu tree string given above
             index (int): Index of final selection
             selection (str): Key of final selection
-            
+
     Options:
         index (int or None): Index to start the menu at, or None for top level
         mark_category (bool): If True, marks categories with a [+] symbol in the menu
         go_back (bool): If True, present an option to go back to previous categories
         start_text (str): Text to display at the top level of the menu
         cmd_on_exit(str): Command to enter when the menu exits - 'look' by default
-        
-    
+
+
     Notes:
         This function will initialize an instance of EvMenu with options generated
         dynamically from the source string, and passes the menu user's selection to
         a function of your choosing. The EvMenu is made of a single, repeating node,
         which will call itself over and over at different levels of the menu tree as
         categories are selected.
-        
+
         Once a non-category selection is made, the user's selection will be passed to
         the given callable, both as a string and as an index number. The index is given
         to ensure every selection has a unique identifier, so that selections with the
         same key in different categories can be distinguished between.
-        
+
         The menus called by this function are not persistent and cannot perform
         complicated tasks like prompt for arbitrary input or jump multiple category
         levels at once - you'll have to use EvMenu itself if you want to take full
         advantage of its features.
-    """  
-    
+    """
+
     # Pass kwargs to store data needed in the menu
     kwargs = {
     "index":index,
@@ -212,7 +212,7 @@ def init_tree_selection(treestr, caller, callback,
     "callback":callback,
     "start_text":start_text
     }
-    
+
     # Initialize menu of selections
     evmenu.EvMenu(caller, "evennia.contrib.tree_select", startnode="menunode_treeselect",
                   startnode_input=None, cmd_on_exit=cmd_on_exit, **kwargs)
@@ -221,10 +221,10 @@ def dashcount(entry):
     """
     Counts the number of dashes at the beginning of a string. This
     is needed to determine the depth of options in categories.
-    
+
     Args:
         entry (str): String to count the dashes at the start of
-        
+
     Returns:
         dashes (int): Number of dashes at the start
     """
@@ -240,11 +240,11 @@ def is_category(treestr, index):
     """
     Determines whether an option in a tree string is a category by
     whether or not there are additional options below it.
-    
+
     Args:
         treestr (str): Multi-line string representing menu options
         index (int): Which line of the string to test
-        
+
     Returns:
         is_category (bool): Whether the option is a category
     """
@@ -262,11 +262,11 @@ def parse_opts(treestr, category_index=None):
     the menu. If category_index corresponds to a category, returns a list
     of options under that category. If category_index corresponds to
     an option that is not a category, it's a selection and returns True.
-    
+
     Args:
         treestr (str): Multi-line string representing menu options
         category_index (int): Index of category or None for top level
-        
+
     Returns:
         kept_opts (list or True): Either a list of options in the selected
                                   category or True if a selection was made
@@ -274,7 +274,7 @@ def parse_opts(treestr, category_index=None):
     dash_depth = 0
     opt_list = treestr.split('\n')
     kept_opts = []
-    
+
     # If a category index is given
     if category_index != None:
         # If given index is not a category, it's a selection - return True.
@@ -284,7 +284,7 @@ def parse_opts(treestr, category_index=None):
         dash_depth = dashcount(opt_list[category_index]) + 1
         # Delete everything before the category index
         opt_list = opt_list [category_index+1:]
-    
+
     # Keep every option (referenced by index) at the appropriate depth
     cur_index = 0
     for option in opt_list:
@@ -298,20 +298,20 @@ def parse_opts(treestr, category_index=None):
             return kept_opts
         cur_index += 1
     return kept_opts
-    
+
 def index_to_selection(treestr, index, desc=False):
     """
     Given a menu tree string and an index, returns the corresponding selection's
     name as a string. If 'desc' is set to True, will return the selection's
     description as a string instead.
-    
+
     Args:
         treestr (str): Multi-line string representing menu options
         index (int): Index to convert to selection key or description
-        
+
     Options:
         desc (bool): If true, returns description instead of key
-        
+
     Returns:
         selection (str): Selection key or description if 'desc' is set
     """
@@ -332,16 +332,16 @@ def index_to_selection(treestr, index, desc=False):
         return selection[0]
     else:
         return selection[1]
-        
+
 def go_up_one_category(treestr, index):
     """
     Given a menu tree string and an index, returns the category that the given option
     belongs to. Used for the 'go back' option.
-    
+
     Args:
         treestr (str): Multi-line string representing menu options
         index (int): Index to determine the parent category of
-        
+
     Returns:
         parent_category (int): Index of parent category
     """
@@ -350,7 +350,7 @@ def go_up_one_category(treestr, index):
     dash_level = dashcount(opt_list[index])
     # Delete everything after the current index
     opt_list = opt_list[:index+1]
-    
+
 
     # If there's no dash, return 'None' to return to base menu
     if dash_level == 0:
@@ -361,25 +361,25 @@ def go_up_one_category(treestr, index):
         if dashcount(selection) == dash_level - 1:
             return current_index
         current_index -= 1
-        
+
 def optlist_to_menuoptions(treestr, optlist, index, mark_category, go_back):
     """
     Takes a list of options processed by parse_opts and turns it into
     a list/dictionary of menu options for use in menunode_treeselect.
-    
+
     Args:
         treestr (str): Multi-line string representing menu options
         optlist (list): List of options to convert to EvMenu's option format
         index (int): Index of current category
         mark_category (bool): Whether or not to mark categories with [+]
         go_back (bool): Whether or not to add an option to go back in the menu
-        
+
     Returns:
         menuoptions (list of dicts): List of menu options formatted for use
             in EvMenu, each passing a different "newindex" kwarg that changes
             the menu level or makes a selection
     """
-    
+
     menuoptions = []
     cur_index = 0
     for option in optlist:
@@ -410,12 +410,12 @@ def optlist_to_menuoptions(treestr, optlist, index, mark_category, go_back):
 def menunode_treeselect(caller, raw_string, **kwargs):
     """
     This is the repeating menu node that handles the tree selection.
-    """  
-    
+    """
+
     # If 'newindex' is in the kwargs, change the stored index.
     if "newindex" in kwargs:
         caller.ndb._menutree.index = kwargs["newindex"]
-    
+
     # Retrieve menu info
     index = caller.ndb._menutree.index
     mark_category = caller.ndb._menutree.mark_category
@@ -423,10 +423,10 @@ def menunode_treeselect(caller, raw_string, **kwargs):
     treestr = caller.ndb._menutree.treestr
     callback = caller.ndb._menutree.callback
     start_text = caller.ndb._menutree.start_text
-    
+
     # List of options if index is 'None' or category, or 'True' if a selection
     optlist = parse_opts(treestr, category_index=index)
-    
+
     # If given index returns optlist as 'True', it's a selection. Pass to callback and end the menu.
     if optlist == True:
         selection = index_to_selection(treestr, index)
@@ -434,10 +434,10 @@ def menunode_treeselect(caller, raw_string, **kwargs):
             callback(caller, treestr, index, selection)
         except Exception:
             log_trace("Error in tree selection callback.")
-        
+
         # Returning None, None ends the menu.
         return None, None
-        
+
     # Otherwise, convert optlist to a list of menu options.
     else:
         options = optlist_to_menuoptions(treestr, optlist, index, mark_category, go_back)
@@ -485,7 +485,7 @@ NAMECOLOR_MENU = """Set name color: Choose a color for your name!
 --Lavender: |535Set your name to Lavender|n
 --Fuchsia: |503Set your name to Fuchsia|n
 Remove name color: Remove your name color, if any"""
-    
+
 class CmdNameColor(Command):
     """
     Set or remove a special color on your name. Just an example for the
@@ -503,7 +503,7 @@ class CmdNameColor(Command):
 def change_name_color(caller, treestr, index, selection):
     """
     Changes a player's name color.
-    
+
     Args:
         caller (obj): Character whose name to color.
         treestr (str): String for the color change menu - unused
@@ -511,11 +511,11 @@ def change_name_color(caller, treestr, index, selection):
         selection (str): Selection made from the name color menu - used
             to determine the color the player chose.
     """
-    
+
     # Store the caller's uncolored name
     if not caller.db.uncolored_name:
         caller.db.uncolored_name = caller.key
-    
+
     # Dictionary matching color selection names to color codes
     colordict = { "Red":"|511", "Pink":"|533", "Maroon":"|301",
     "Orange":"|531", "Brown":"|321", "Sienna":"|420",
@@ -523,7 +523,7 @@ def change_name_color(caller, treestr, index, selection):
     "Green":"|141", "Lime":"|350", "Forest":"|032",
     "Blue":"|115", "Cyan":"|155", "Navy":"|113",
     "Purple":"|415", "Lavender":"|535", "Fuchsia":"|503"}
-    
+
     # I know this probably isn't the best way to do this. It's just an example!
     if selection == "Remove name color": # Player chose to remove their name color
         caller.key = caller.db.uncolored_name
