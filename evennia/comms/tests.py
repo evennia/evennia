261d7e84e2a59c6b499c7ac8fdb29bdd0a099a4f
ChrisLR <arzhul@gmail.com>
2018-05-04 19:33:56 -0400

    Simplify channel creation in tests.

--- evennia/comms/tests.py
+++ evennia/comms/tests.py
@@ -5,15 +5,13 @@ from evennia.utils.test_resources import EvenniaTest
 
 class HiddenChannelTest(EvenniaTest):
     def test_hidden_channel_not_returned_by_manager(self):
-        hidden_channel = ChannelDB(is_hidden=True)
-        hidden_channel.save()
+        hidden_channel = ChannelDB.objects.create(is_hidden=True)
         channels = ChannelDB.objects.get_all_channels()
 
         assert hidden_channel not in channels
 
     def test_hidden_channel_returned_with_arg(self):
-        hidden_channel = ChannelDB(is_hidden=True)
-        hidden_channel.save()
+        hidden_channel = ChannelDB.objects.create(is_hidden=True)
         channels = ChannelDB.objects.get_all_channels(include_hidden=True)
 
         assert hidden_channel in channels
