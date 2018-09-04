da60d1ed457c8ac8fb3818d89cef73a83f5bb9d2
Johnny <strikax@gmail.com>
2018-09-04 21:48:03 +0000

    Allows more configurable extensibility and addresses PR change requests.

--- evennia/contrib/auditing/tests.py
+++ evennia/contrib/auditing/tests.py
@@ -5,9 +5,10 @@ Module containing the test cases for the Audit system.
 from django.conf import settings
 from evennia.contrib.auditing.server import AuditedServerSession
 from evennia.utils.test_resources import EvenniaTest
+import re
 
 # Configure session auditing settings
-settings.AUDIT_CALLBACK = "evennia.contrib.auditing.examples"
+settings.AUDIT_CALLBACK = "evennia.contrib.auditing.outputs.to_syslog"
 settings.AUDIT_IN = True
 settings.AUDIT_OUT = True
 
@@ -22,11 +23,19 @@ class AuditingTest(EvenniaTest):
         information from strings.
         """
         safe_cmds = (
-            'say hello to my little friend',
+            '/say hello to my little friend',
             '@ccreate channel = for channeling',
+            '@create/drop some stuff',
+            '@create rock',
             '@create a pretty shirt : evennia.contrib.clothing.Clothing',
             '@charcreate johnnyefhiwuhefwhef',
             'Command "@logout" is not available. Maybe you meant "@color" or "@cboot"?',
+            '/me says, "what is the password?"',
+            'say the password is plugh',
+            # Unfortunately given the syntax, there is no way to discern the
+            # latter of these as sensitive
+            '@create pretty sunset'
+            '@create johnny password123',
         )
         
         for cmd in safe_cmds:
@@ -34,15 +43,32 @@ class AuditingTest(EvenniaTest):
             
         unsafe_cmds = (
             ('connect johnny password123', 'connect johnny ***********'),
-            ('concnct johnny password123', 'concnct *******************'),
+            ('concnct johnny password123', 'concnct johnny ***********'),
+            ('concnct johnnypassword123', 'concnct *****************'),
+            ('connect "johnny five" "password 123"', 'connect "johnny five" **************'),
+            ('connect johnny "password 123"', 'connect johnny **************'),
             ('create johnny password123', 'create johnny ***********'),
-            ('@userpassword johnny = password234', '@userpassword johnny ***********'),
+            ('@password password1234 = password2345', '@password ***************************'),
+            ('@password password1234 password2345', '@password *************************'),
+            ('@passwd password1234 = password2345', '@passwd ***************************'),
+            ('@userpassword johnny = password234', '@userpassword johnny = ***********'),
             ('craete johnnypassword123', 'craete *****************'),
-            ("Command 'conncect teddy teddy' is not available. Maybe you meant \"@encode\"?", 'Command *************************************************************************************')
+            ("Command 'conncect teddy teddy' is not available. Maybe you meant \"@encode\"?", 'Command \'conncect ***** *****\' is not available. Maybe you meant "@encode"?'),
+            ("{'text': u'Command \\'conncect jsis dfiidf\\' is not available. Type \"help\" for help.'}", "{'text': u'Command \\'conncect jsis ******\\' is not available. Type \"help\" for help.'}")
         )
         
-        for unsafe, safe in unsafe_cmds:
-            self.assertEqual(self.session.mask(unsafe), safe)
+        for index, (unsafe, safe) in enumerate(unsafe_cmds):
+            self.assertEqual(re.sub('<Masked: .+>', '', self.session.mask(unsafe)).strip(), safe)
+            
+        # Make sure scrubbing is not being abused to evade monitoring
+        secrets = [
+            'say password password password; ive got a secret that i cant explain',
+            'whisper johnny = password let\'s lynch the landlord',
+            'say connect johnny password1234 secret life of arabia',
+            "@password;eval(\"__import__('os').system('clear')\", {'__builtins__':{}})"
+        ]
+        for secret in secrets:
+            self.assertEqual(self.session.mask(secret), secret)
         
     def test_audit(self):
         """
@@ -60,5 +86,5 @@ class AuditingTest(EvenniaTest):
         
         # Make sure auditor is breaking down responses without actual text
         log = self.session.audit(**{'logged_in': {}, 'src': 'server'})
-        self.assertEqual(log['msg'], 'logged_in')
+        self.assertEqual(log['msg'], "{'logged_in': {}}")
     
