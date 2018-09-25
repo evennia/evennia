e51c48c8fce70faaa1ea1328686c5b556a8ddcec
Griatch <griatch@gmail.com>
2018-09-25 20:07:11 +0200

    Make Docker image support develop branch launcher

--- bin/unix/evennia-docker-start.sh
+++ bin/unix/evennia-docker-start.sh
@@ -7,7 +7,4 @@ rm /usr/src/game/server/*.pid >& /dev/null || true
 
 # start evennia server; log to server.log but also output to stdout so it can 
 # be viewed with docker-compose logs
-exec 3>&1; evennia start 2>&1 1>&3 | tee /usr/src/game/server/logs/server.log; exec 3>&-
-
-# start a shell to keep the container running
-bash
+exec 3>&1; evennia start -l
