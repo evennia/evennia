1d556959593ff25c883fdbe765402d866fd1934b
Johnny <strikax@gmail.com>
2018-09-26 20:51:24 +0000

    Fixes infinite recursion vulnerability, adds distance limit to LOS(), adds docstrings, tweaks export_json(), simplifies script deployment.

--- evennia/contrib/pathfinding/scripts.py
+++ evennia/contrib/pathfinding/scripts.py
@@ -1,13 +1,80 @@
+"""
+PathfinderScript
+
+Contrib - Johnny 2018
+
+A batteries-included abstraction for in-MUD navigation.
+
+Upon initialization, the Pathfinder will quietly build a "map" (directional 
+graph) of all of your game's Rooms and Exits, even accounting for one-way 
+travel.
+
+Requires the `networkx` library, not included with Evennia, but only a
+`pip install networkx` away.
+
+---
+
+Installation:
+
+Get or create an instance of this script at server start/reload.
+
+To do so, modify `yourgame/server/conf/at_server_startstop.py`:
+
+Add this import to the top of the file:
+
+    from evennia.contrib.pathfinding.scripts import PathfinderScript
+    
+Add this line to the beginning or end of your `at_server_start()` function, but
+above the word 'pass' if it's there:
+
+    # This will preload graph creation before the game is live
+    pathfinder = PathfinderScript.spawn()
+    
+That's it! It is now running and will update itself every 30 minutes.
+
+To use it:
+
+    from evennia.contrib.pathfinding.scripts import PathfinderScript
+    pfind = PathfinderScript.spawn()
+    
+    pfind.map.get_directions(room_obj1, room_obj2)
+    >> ['down', 'east', 'down', 'north', 'down']
+    
+"""
+
 from evennia import DefaultScript
 from evennia.contrib.pathfinding.pathfinder import Pathfinder
-from evennia.utils import logger
+from evennia.utils import create, logger
 
 class PathfinderScript(DefaultScript):
     """Generates and maintains a global Pathfinder instance."""
     
     @property
-    def graph(self):
+    def map(self):
+        """
+        Returns a reference to the stored Pathfinder.
+        """
         return self.db.pathfinder
+        
+    @classmethod
+    def spawn(cls, **kwargs):
+        """
+        Gets the first existing script instance, or creates new
+        
+        Kwargs:
+            any (any): Forwarded to create.create_script
+            
+        Returns:
+            obj (PathfinderScript): PathfinderScript
+        """
+        if kwargs:
+            obj = cls.objects.get(**kwargs)
+        else:
+            obj = cls.objects.first()
+            
+        kwargs['obj'] = None
+        if not obj: obj = create.create_script(cls, **kwargs)
+        return obj
     
     def at_script_creation(self):
         self.key = "pathfinder"
--- evennia/contrib/pathfinding/tests.py
+++ evennia/contrib/pathfinding/tests.py
@@ -8,16 +8,13 @@ class PathfinderTest(EvenniaTest):
     
     def test_script(self):
         "Tests handling of script"
-        # Initialize script globally
-        obj = create_script('evennia.contrib.pathfinding.scripts.PathfinderScript', key='pathfinder', obj=None)
-        self.assertTrue(obj, 'Pathfinder script was not created.')
         
-        # Recall the script as an obj
-        pf_script = next((x for x in PathfinderScript.objects.all() if x.key == 'pathfinder'), None)
+        # Initialize the script as an obj
+        pf_script = PathfinderScript.spawn()
         self.assertTrue(pf_script, 'The Pathfinder script was not retrieved.')
         
         # Ask script for directions
-        directions = pf_script.graph.get_directions(self.basement, self.bedmaster)
+        directions = pf_script.map.get_directions(self.basement, self.bedmaster)
         self.assertTrue(directions, 'No directions were returned!')
         self.assertTrue(len(directions) > 2, 'Too few directions were returned!')
     
@@ -25,6 +22,10 @@ class PathfinderTest(EvenniaTest):
         "Get path from Kitchen to Lair"
         path = self.pfinder.get_path(self.kitchen, self.lair)
         self.assertTrue(path, 'No path was computed from kitchen to lair.')
+        
+        # Test path that doesn't exist
+        path = self.pfinder.get_path(self.lair, self.stairs)
+        self.assertFalse(path, 'There is no path from the Lair to the Infinite Stairs. %s should not have been returned.' % path)
         
         # Get path from bedroom to treasure room
         path = self.pfinder.get_path(self.bed1, self.treasure_room)
@@ -37,10 +38,25 @@ class PathfinderTest(EvenniaTest):
         self.assertEqual(list(path), correct, "Did not return the correct path.")
         
     def test_get_line_of_sight(self):
-        "Returns all objects in line of sight from source"
+        "Returns all Room objects in line of sight from source"
         objs = self.pfinder.get_line_of_sight(self.kitchen, 'down')
         self.assertTrue(objs)
         self.assertEqual(2, len(objs))
+        
+        # Test line of sight for invalid direction
+        objs = self.pfinder.get_line_of_sight(self.kitchen, 'yonder')
+        self.assertTrue(len(objs) == 1, 'There is no line of sight from Kitchen to "yonder"; this should not have returned anything further than the Kitchen (%s).' % objs)
+        
+        # Make sure distance limiter works.
+        caboose = self.dungeon_train[0]
+        objs = self.pfinder.get_line_of_sight(caboose, 'east', 3)
+        self.assertEqual(len(objs), 3, 'Player should only be able to see 3 cars ahead on the Dungeon Train.')
+        
+        # Make sure we break out of circular paths
+        stairs = self.stairs
+        # Set distance to something obscenely high so the counter doesn't get in the way
+        objs = self.pfinder.get_line_of_sight(stairs, 'up', 9999999999)
+        self.assertEqual(len(objs), 1, 'Infinite stairs are unacceptable!')
         
     def test_json_export(self):
         "Should return a JSON serialized graph."
@@ -124,6 +140,17 @@ class PathfinderTest(EvenniaTest):
         
         # Lair to yard
         create.create_object(self.exit_typeclass, key='up', location=self.lair, destination=self.frontyard)
+        
+        # Create a linear set of rooms with unidirectional exits
+        dungeon_train = self.dungeon_train = [create.create_object(self.room_typeclass, key="Dungeon Train Railcar #%i" % x, nohome=True) for x in xrange(10)]
+        car_pairs = zip(dungeon_train[0:], dungeon_train[1:])
+        for source, dest in car_pairs:
+            create.create_object(self.exit_typeclass, key='east', location=source, destination=dest)
+            
+        # Create a circular path
+        stairs = self.stairs = create.create_object(self.room_typeclass, key="Staircase", nohome=True)
+        create.create_object(self.exit_typeclass, key='up', location=stairs, destination=stairs)
+        create.create_object(self.exit_typeclass, key='down', location=stairs, destination=stairs)
         
         # map is a reserved keyword
         self.pfinder = Pathfinder()
