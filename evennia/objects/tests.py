84f1cad6a250411c3ff8707870fbb13a78517780
Johnny <strikax@gmail.com>
2018-10-22 20:33:56 +0000

    Renames methods to web_get_* and fixes tests.

--- evennia/objects/tests.py
+++ evennia/objects/tests.py
@@ -5,7 +5,7 @@ class DefaultObjectTest(EvenniaTest):
     def test_urls(self):
         "Make sure objects are returning URLs"
         self.assertTrue(self.char1.get_absolute_url())
-        self.assertTrue('admin' in self.char1.get_admin_url())
+        self.assertTrue('admin' in self.char1.web_get_admin_url())
         
         self.assertTrue(self.room1.get_absolute_url())
-        self.assertTrue('admin' in self.room1.get_admin_url())
+        self.assertTrue('admin' in self.room1.web_get_admin_url())
