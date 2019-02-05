3188f6643685a07b50abcb0d915c4c790d99ca0e
Griatch <griatch@gmail.com>
2019-02-06 00:10:26 +0100

    Fix test typo

--- evennia/commands/default/tests.py
+++ evennia/commands/default/tests.py
@@ -350,8 +350,8 @@ class TestBuilding(CommandTest):
         self.call(building.CmdSetAttribute(), "Obj2/test2=\"value2\"", "Created attribute Obj2/test2 = 'value2'")
 
         self.call(building.CmdCpAttr(), "/copy Obj2/test2 = Obj2/test3",
-                  "@cpattr: Extra switch \"/copy\" ignored.| Copied Obj2.test2 -> Obj2.test3. "
-                  "(value: 'value2')Copied Obj2.test1 -> Obj2.test3")
+                  "@cpattr: Extra switch \"/copy\" ignored.|\nCopied Obj2.test2 -> Obj2.test3. "
+                  "(value: 'value2'))
         self.call(building.CmdMvAttr(), "Obj2/test2 = Obj/test3", "Moved Obj2.test2 -> Obj.test3")
         self.call(building.CmdMvAttr(), "", "Usage: ")
         self.call(building.CmdCpAttr(), "Obj/test1 = Obj2/test3", "Copied Obj.test1 -> Obj2.test3")
