007c8fce922564a72b1a258f11655083bf719f4e
Griatch <griatch@gmail.com>
2019-06-28 22:22:29 +0200

    Some more info about the typeclass/list cmd in docstring

--- evennia/commands/default/building.py
+++ evennia/commands/default/building.py
@@ -1691,27 +1691,28 @@ class CmdTypeclass(COMMAND_DEFAULT_CLASS):
               object - basically making this a new clean object.
       force - change to the typeclass also if the object
               already has a typeclass of the same name.
-      list - show available typeclasses.
-
+      list - show available typeclasses. Only typeclasses in modules actually
+             imported or used from somewhere in the code will show up here
+             (those typeclasses are still available if you know the path)
 
     Example:
       type button = examples.red_button.RedButton
 
-    If the typeclass_path is not given, the current object's
-    typeclass is assumed.
+    If the typeclass_path is not given, the current object's typeclass is
+    assumed.
 
-    View or set an object's typeclass. If setting, the creation hooks
-    of the new typeclass will be run on the object. If you have
-    clashing properties on the old class, use /reset. By default you
-    are protected from changing to a typeclass of the same name as the
-    one you already have - use /force to override this protection.
+    View or set an object's typeclass. If setting, the creation hooks of the
+    new typeclass will be run on the object. If you have clashing properties on
+    the old class, use /reset. By default you are protected from changing to a
+    typeclass of the same name as the one you already have - use /force to
+    override this protection.
 
-    The given typeclass must be identified by its location using
-    python dot-notation pointing to the correct module and class. If
-    no typeclass is given (or a wrong typeclass is given). Errors in
-    the path or new typeclass will lead to the old typeclass being
-    kept. The location of the typeclass module is searched from the
-    default typeclass directory, as defined in the server settings.
+    The given typeclass must be identified by its location using python
+    dot-notation pointing to the correct module and class. If no typeclass is
+    given (or a wrong typeclass is given). Errors in the path or new typeclass
+    will lead to the old typeclass being kept. The location of the typeclass
+    module is searched from the default typeclass directory, as defined in the
+    server settings.
 
     """
 
