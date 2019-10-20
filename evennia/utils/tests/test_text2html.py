7ceef9a54ad686b41e8b31b6c79df6e421f01bc3
Henddher  Pedroza <henddher.pedroza@gmail.com>
2019-10-19 21:55:32 -0400

    Increase coverage in evennia/utils/text2html.

--- evennia/utils/tests/test_text2html.py
+++ evennia/utils/tests/test_text2html.py
@@ -13,6 +13,12 @@ class TestText2Html(TestCase):
         self.assertEqual(
             "<span class=\"err\">red</span>foo",
             parser.re_color(ansi.ANSI_RED + "red" + ansi.ANSI_NORMAL + "foo"))
+        self.assertEqual(
+            "<span class=\"bgcolor-001\">red</span>foo",
+            parser.re_color(ansi.ANSI_BACK_RED + "red" + ansi.ANSI_NORMAL + "foo"))
+        self.assertEqual(
+            "<span class=\"bgcolor-001\"><span class=\"err\">red</span></span>foo",
+            parser.re_color(ansi.ANSI_BACK_RED + ansi.ANSI_GREEN + "red" + ansi.ANSI_NORMAL + "foo"))
 
     def test_re_bold(self):
         parser = text2html.HTML_PARSER
