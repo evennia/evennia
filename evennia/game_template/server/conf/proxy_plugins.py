35d3402b07900941540fdbaeddad6c584b394411
trhr <sage@limitlessinteractive.com>
2020-02-10 00:34:09 -0600

    Proxy plugin module

---
+++ evennia/game_template/server/conf/proxy_plugins.py
@@ -0,0 +1,13 @@
+"""
+Proxy plugin hooks.
+"""
+
+
+def start_proxy_service(web_root):
+    """
+    This plugin module can define user-created services for the Portal to
+    start. This is necessary for exposing web-based protocols.
+
+
+    """
+    return web_root
