8807fccc4df0f1a6101a8413167169dac7b4c6ef
Griatch <griatch@gmail.com>
2020-04-05 11:43:25 +0200

    Don't include tags for multiversion docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,6 +88,7 @@ html_sidebars = {
 
 # sphinx-multiversion config
 
+smv_tag_whitelist = None
 smv_branch_whitelist = r"^static-file-docs$"
 smv_outputdir_format = "versions" + sep + "{config.release}"
 
