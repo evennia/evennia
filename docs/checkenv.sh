c29669c4846ac3ff4fddb9caaf7ca5c99724287f
Griatch <griatch@gmail.com>
2020-04-05 13:22:51 +0200

    Less checkenv output

--- docs/checkenv.sh
+++ docs/checkenv.sh
@@ -7,16 +7,12 @@ if [ ! -d "$EVDIR" ]; then
 
   echo "The evennia dir is not found at $EVDIR.";
   exit 1
-else
-  echo "Evennia dir correctly set to $EVDIR.";
 fi 
 
 if [ ! -d "$EVGAMEDIR" ]; then
 
     echo "The gamedir is not found at $EVGAMEDIR";
     exit 1
-else
-  echo "Evennia game dir correctly set to $EVGAMEDIR.";
 fi 
 
 if [ $# -ne 0 ]
