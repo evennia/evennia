fa7ebc41def8caa163c8ba841301e82343164f76
Griatch <griatch@gmail.com>
2020-04-05 13:00:01 +0200

    Changes to docs setup

--- docs/checkenv.sh
+++ docs/checkenv.sh
@@ -1,11 +1,7 @@
 
 # check environment
 
-
-# if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
-#   echo "There are uncommitted changes. Make sure to commit everything in your current branch first."
-#   exit 1
-# fi
+# common checks 
 
 if [ ! -d "$EVDIR" ]; then
 
@@ -22,3 +18,17 @@ if [ ! -d "$EVGAMEDIR" ]; then
 else
   echo "Evennia game dir correctly set to $EVGAMEDIR.";
 fi 
+
+if [ $# -ne 0 ]
+
+  # a multi-version build
+  
+  then 
+
+    if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
+      echo "There are uncommitted changes. Make sure to commit everything in your current branch before doing a multiversion build."
+      exit 1
+    fi
+
+fi
+
