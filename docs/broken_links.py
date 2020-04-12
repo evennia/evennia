3d26d249057da6d071cbd51fefbbc405c55fe9e1
trhr <sage@limitlessinteractive.com>
2020-04-12 00:49:21 -0500

    More changes

--- docs/broken_links.py
+++ docs/broken_links.py
@@ -7,7 +7,8 @@ from colorama import Fore, Back, Style
 files = list()
 os.chdir("source")
 path = os.getcwd()
-broken_count=0
+broken_count = 0
+clean_count = 0
 
 parser = argparse.ArgumentParser(description='Check broken links')
 parser.add_argument('--verbose', default=False, help="Verbosity level (1: Show all links)")
@@ -19,20 +20,24 @@ def shortpath(path):
 
 def check_broken_links(file):
     global broken_count
+    global clean_count
     with open(file,"r") as f:
 #        matches = re.findall(r"(\[.*\])\(((?!http)[^\)]*)\)",f.read())
-        matches = re.findall(r"(\[.*\])\(((?!http)[^#\)]*)([#A-Za-z0-9]*)\)",f.read())
+#        matches = re.findall(r"(\[.*\])\(((?!http)[^#\)]*)([A-Za-z0-9]*)\)",f.read())
+        matches = re.findall(r"\[([^\]]+)\]\((?!http)([^[\)#]+)",f.read())
         if matches:
             print(Fore.BLUE, "{file}...".format(file=shortpath(file)), Style.RESET_ALL)
             for match in matches:
                 if match[1]:
+                    extra = match[2] if len(match)>=3 else ""
                     link_dest = os.path.realpath(os.path.join(os.path.dirname(file), match[1] +".md"))
                     if not os.path.exists(link_dest):
-                        print("\t\t", Back.RED, Fore.WHITE, "Broken : {link}".format(link=shortpath(link_dest)), Style.RESET_ALL)
+                        print("\t\t", Back.RED, Fore.WHITE, "Broken : {link}{extra}".format(link=shortpath(link_dest),extra=extra), Style.RESET_ALL)
                         broken_count += 1
                     else:
                         if args.verbose:
-                            print(Fore.GREEN, "\t\tClean : {link}".format(link=shortpath(link_dest)), Style.RESET_ALL)
+                            print(Fore.GREEN, "\t\tClean : {link}{extra}".format(link=shortpath(link_dest), extra=extra), Style.RESET_ALL)
+                            clean_count += 1
         else:
             if args.verbose:
                 print(Fore.RED, "{file} has no links".format(file=shortpath(file)), Style.RESET_ALL)
@@ -50,4 +55,6 @@ def get_md_files(path):
 
 print("Starting broken link checker...")
 get_md_files(path)
+print("Done.")
 print("Broken links: {num}".format(num=broken_count))
+print("Clean links: {num}".format(num=clean_count))
