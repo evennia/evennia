5004a818d09a403e6f80857476339bbb78868cc7
Griatch <griatch@gmail.com>
2020-06-13 00:40:36 +0200

    Further tweak to deployment

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -10,21 +10,25 @@ if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
   exit 1
 fi
 
+# get the deployment branch
 git checkout gh-pages
 
 mv build ..
+# the docs/ folder is named the same in both branches so we need to step out of 
+# it to not confuse what's what.
 cd ..
 rm -Rf docs/*
-cp -Rf build/html/* docs/
+cp -Rf build/html/versions/* docs/
 git add docs/*
 rm -Rf build
 
 git commit -a -m "Updated HTML docs"
+
 echo "Skipping deployment"
 # git push origin gh-pages
 
-# get back to previous branch
-
- git checkout -
+# get back to previous branch (go into docs/ so we end up where we started)
+cd docs 
+git checkout -
 
 echo "Deployed to https://evennia.github.io/evennia-docs."
