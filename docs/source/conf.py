548ffa18abe59c94540a4666aa1cf12e0a3e4f75
Griatch <griatch@gmail.com>
2020-06-13 23:10:42 +0200

    Move back the _static/_templates dirs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,11 +68,11 @@ extensions = [
 autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['_templates']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
 
 # Custom extras for sidebar
 html_sidebars = {
