5e6c52e2e64decdc6e9c019a59462384b9213f87
Griatch <griatch@gmail.com>
2020-06-26 21:17:07 +0200

    Fix for typeclass app_label, and admin fix. Resolves #2112.

--- evennia/accounts/admin.py
+++ evennia/accounts/admin.py
@@ -4,12 +4,25 @@
 #
 from django import forms
 from django.conf import settings
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.admin.options import IS_POPUP_VAR
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.admin.utils import unquote
+from django.template.response import TemplateResponse
+from django.http import Http404, HttpResponseRedirect
+from django.core.exceptions import PermissionDenied
+from django.views.decorators.debug import sensitive_post_parameters
+from django.utils.decorators import method_decorator
+from django.utils.html import escape
+from django.urls import path, reverse
+from django.contrib.auth import update_session_auth_hash
+
 from evennia.accounts.models import AccountDB
 from evennia.typeclasses.admin import AttributeInline, TagInline
 from evennia.utils import create
+
+sensitive_post_parameters_m = method_decorator(sensitive_post_parameters())
 
 
 # handle the custom User editor
@@ -259,6 +272,71 @@ class AccountDBAdmin(BaseUserAdmin):
             },
         ),
     )
+
+    @sensitive_post_parameters_m
+    def user_change_password(self, request, id, form_url=''):
+        user = self.get_object(request, unquote(id))
+        if not self.has_change_permission(request, user):
+            raise PermissionDenied
+        if user is None:
+            raise Http404('%(name)s object with primary key %(key)r does not exist.') % {
+                'name': self.model._meta.verbose_name,
+                'key': escape(id),
+            }
+        if request.method == 'POST':
+            form = self.change_password_form(user, request.POST)
+            if form.is_valid():
+                form.save()
+                change_message = self.construct_change_message(request, form, None)
+                self.log_change(request, user, change_message)
+                msg = 'Password changed successfully.'
+                messages.success(request, msg)
+                update_session_auth_hash(request, form.user)
+                return HttpResponseRedirect(
+                    reverse(
+                        '%s:%s_%s_change' % (
+                            self.admin_site.name,
+                            user._meta.app_label,
+                            # the model_name is something we need to hardcode
+                            # since our accountdb is a proxy:
+                            "accountdb",
+                        ),
+                        args=(user.pk,),
+                    )
+                )
+        else:
+            form = self.change_password_form(user)
+
+        fieldsets = [(None, {'fields': list(form.base_fields)})]
+        adminForm = admin.helpers.AdminForm(form, fieldsets, {})
+
+        context = {
+            'title': 'Change password: %s' % escape(user.get_username()),
+            'adminForm': adminForm,
+            'form_url': form_url,
+            'form': form,
+            'is_popup': (IS_POPUP_VAR in request.POST or
+                         IS_POPUP_VAR in request.GET),
+            'add': True,
+            'change': False,
+            'has_delete_permission': False,
+            'has_change_permission': True,
+            'has_absolute_url': False,
+            'opts': self.model._meta,
+            'original': user,
+            'save_as': False,
+            'show_save': True,
+            **self.admin_site.each_context(request),
+        }
+
+        request.current_app = self.admin_site.name
+
+        return TemplateResponse(
+            request,
+            self.change_user_password_template or
+            'admin/auth/user/change_password.html',
+            context,
+        )
 
     def save_model(self, request, obj, form, change):
         """
