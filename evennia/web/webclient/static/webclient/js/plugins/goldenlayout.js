a8ab09e72ddb989c69e5ff95d766f4d3d0b6ce8a
Griatch <griatch@gmail.com>
2020-09-27 23:54:10 +0200

    Start tutorialmenu

--- evennia/web/webclient/static/webclient/js/plugins/goldenlayout.js
+++ evennia/web/webclient/static/webclient/js/plugins/goldenlayout.js
@@ -6,7 +6,7 @@
 let goldenlayout = (function () {
 
     var myLayout;
-    var knownTypes = ["all", "untagged"];
+    var knownTypes = ["all", "untagged", "testing"];
     var untagged = [];
 
     var newTabConfig = {
