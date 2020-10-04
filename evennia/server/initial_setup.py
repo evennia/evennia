a94e723d6b285aed1418055a1fdcc32cf0924c57
Griatch <griatch@gmail.com>
2020-10-04 19:50:49 +0200

    Add intro-menu to tutorial-world

--- evennia/server/initial_setup.py
+++ evennia/server/initial_setup.py
@@ -29,7 +29,7 @@ LIMBO_DESC = _(
     """
 Welcome to your new |wEvennia|n-based game! Visit http://www.evennia.com if you need
 help, want to contribute, report issues or just join the community.
-As Account #1 you can create a demo/tutorial area with |w@batchcommand tutorial_world.build|n.
+As Account #1 you can create a demo/tutorial area with '|wbatchcommand tutorial_world.build|n'.
     """
 )
 
