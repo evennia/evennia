d126d5cc322e7e5ca6cd2dc863c894dc899b22b9
Griatch <griatch@gmail.com>
2020-10-14 16:01:40 +0200

    Re-revert of lockfunc change because I'm a dummy, as per #2228

--- evennia/locks/lockfuncs.py
+++ evennia/locks/lockfuncs.py
@@ -539,6 +539,8 @@ def objtag(accessing_obj, accessed_obj, *args, **kwargs):
     Only true if accessed_obj has the specified tag and optional
     category.
     """
+    if hasattr(accessed_obj, "obj"):
+        accessed_obj = accessed_obj.obj
     tagkey = args[0] if args else None
     category = args[1] if len(args) > 1 else None
     return bool(accessed_obj.tags.get(tagkey, category=category))
