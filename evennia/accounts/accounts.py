c574998539cce601eb7f5e336afa624268f292d2
fariparedes <you@example.com>
2021-03-26 21:34:03 -0400

    Proposed fix

--- evennia/accounts/accounts.py
+++ evennia/accounts/accounts.py
@@ -1498,7 +1498,7 @@ class DefaultAccount(AccountDB, metaclass=TypeclassBase):
                     )
                 )
             result.append("\n\n |whelp|n - more commands")
-            result.append("\n |wooc <Text>|n - talk on public channel")
+            result.append("\n |wpublic <Text>|n - talk on public channel")
 
             charmax = _MAX_NR_CHARACTERS
 
