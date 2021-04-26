c209a9b8ef04e6dc6e9ec7393198ddafd17005b2
Ben Longden <ben.longden@infinityworks.com>
2021-04-26 10:31:42 +0100

    Move prompt logic to a flag set by TTYPE (for Mudlet, currently)

--- evennia/server/portal/suppress_ga.py
+++ evennia/server/portal/suppress_ga.py
@@ -40,6 +40,7 @@ class SuppressGA(object):
         self.protocol = protocol
 
         self.protocol.protocol_flags["NOGOAHEAD"] = True
+        self.protocol.protocol_flags["NOPROMPTGOAHEAD"] = True # Used to send a GA after a prompt line only, set in TTYPE (per client)
         # tell the client that we prefer to suppress GA ...
         self.protocol.will(SUPPRESS_GA).addCallbacks(self.will_suppress_ga, self.wont_suppress_ga)
 
