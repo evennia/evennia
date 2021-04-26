9b749d16a63941a45f017916fc68c2d210b0dc37
Ben Longden <ben.longden@infinityworks.com>
2021-04-26 12:29:05 +0100

    For mudlet we overwrite the negotiated GA value
    Suppress GA
    Activate the NOPROMPTGOAHEAD flag so GA's only sent on prompts

--- evennia/server/portal/ttype.py
+++ evennia/server/portal/ttype.py
@@ -120,6 +120,7 @@ class Ttype(object):
                 # supports xterm256 stably since 1.1 (2010?)
                 xterm256 = clientname.split("MUDLET", 1)[1].strip() >= "1.1"
                 # Mudlet likes GA's on a prompt line for the prompt trigger to match.
+                self.protocol.protocol_flags["NOGOAHEAD"] = True
                 self.protocol.protocol_flags["NOPROMPTGOAHEAD"] = False
 
             if (
