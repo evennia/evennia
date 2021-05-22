4bea4d5703b85b046e43bc466cd5dfbed9b5a3e7
Griatch <griatch@gmail.com>
2021-05-22 16:11:48 +0200

    Continue documentation of web components

--- evennia/game_template/web/urls.py
+++ evennia/game_template/web/urls.py
@@ -1,26 +1,30 @@
 """
-This is the starting point when a user enters a url in their web browser. 
+This is the starting point when a user enters a url in their web browser.
 
 The urls is matched (by regex) and mapped to a 'view' - a Python function or
 callable class that in turn (usually) makes use of a 'template' (a html file
 with slots that can be replaced by dynamic content) in order to render a HTML
 page to show the user.
 
-This file is already set up to correctly handle all of Evennia's existing web
-pages (including the webclient). But if you want to add a new page you needs to
-start add by adding its view to `custom_patterns`. 
+This file includes the urls in website, webclient and admin. To override you
+should modify urls.py in those sub directories.
 
 Search the Django documentation for "URL dispatcher" for more help.
 
 """
-from django.conf.urls import url, include
+from django.conf.urls import path, include
 
 # default evennia patterns
 from evennia.web.urls import urlpatterns as evennia_default_urlpatterns
 
-# add custom patterns here
+# add patterns
 urlpatterns = [
-    # url(r'/desired/url/regex', 'path.to.python.view', name='example'),
+    # website
+    path("", include("web.website.urls")),
+    # webclient
+    path("webclient/", include("web.webclient.urls")),
+    # web admin
+    path("admin/", include("web.admin.urls"))
 ]
 
 # 'urlpatterns' must be named such for Django to find it.
