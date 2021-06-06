bcec7a1ec0e83fcc9fe105d3d4f9897f8a476a82
davewiththenicehat <54369722+davewiththenicehat@users.noreply.github.com>
2021-06-05 21:18:49 -0400

    cleanup

--- evennia/help/models.py
+++ evennia/help/models.py
@@ -19,7 +19,6 @@ from evennia.help.manager import HelpEntryManager
 from evennia.typeclasses.models import Tag, TagHandler, AliasHandler
 from evennia.locks.lockhandler import LockHandler
 from evennia.utils.utils import lazy_property
-from evennia.utils.logger import log_info
 
 __all__ = ("HelpEntry",)
 
@@ -57,9 +56,11 @@ class HelpEntry(SharedMemoryModel):
     db_key = models.CharField(
         "help key", max_length=255, unique=True, help_text="key to search for"
     )
+
     @lazy_property
     def key(self):
         return self.db_key
+
     # help category
     db_help_category = models.CharField(
         "help category",
@@ -67,9 +68,11 @@ class HelpEntry(SharedMemoryModel):
         default="General",
         help_text="organizes help entries in lists",
     )
+
     @lazy_property
     def help_category(self):
         return self.db_help_category
+
     # the actual help entry text, in any formatting.
     db_entrytext = models.TextField(
         "help entry", blank=True, help_text="the main body of help text"
