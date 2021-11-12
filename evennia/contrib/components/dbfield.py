8ca490cccbd6f9ccfa107de662e3586f42744600
ChrisLR <arzhul@gmail.com>
2021-11-12 16:18:53 -0500

    Removed the cache, fixed a few issues with set_name, removed reference to signals

--- evennia/contrib/components/dbfield.py
+++ evennia/contrib/components/dbfield.py
@@ -3,33 +3,21 @@ UNSET = object()
 
 class DBField(object):
     """
-    This is the class to use in components for attributes that bind to their host's db.
+    This is the class to use in components for attributes that bind to their owner's db.
     Allows using a simpler syntax, similar to component.attribute
     Values are stored on the host's db using a prefixed key made of
     the component's name and the attribute name, similar to component__attribute.
-
-    If the parent component's host is not set, values are stored in a local cache.
-    This allows you to create Templates that you can then duplicate to valid hosts.
     """
 
     def __init__(self, default_value=None):
         self.default_value = default_value
-        self._cache = {}
 
     def assign_default_value(self, instance):
-        default_value = self._get_default_value()
-        self._cache[instance] = default_value
+        default_value = self.get_default_value()
+        key = f"{instance.name}__{self.name}"
+        self.handler(instance).add(key, default_value)
 
-        # Lists and dicts have to be set from the start to avoid mutable objects related mistakes
-        if self.default_value is list or self.default_value is dict:
-            host = instance.host
-            key = f"{instance.name}_{self.name}"
-            if host:
-                self.handler(instance).add(key, default_value)
-
-        return default_value
-
-    def _get_default_value(self):
+    def get_default_value(self):
         default_value = self.default_value
         if callable(default_value):
             default_value = default_value()
@@ -40,41 +28,32 @@ class DBField(object):
         if not instance:
             raise NoInstanceError(f"{self.name} can only be retrieved from instances.")
 
-        host = instance.host
-        key = f"{instance.name}_{self.name}"
-        if host:
-            value = self.handler(instance).get(key, UNSET)
-            if value is UNSET or (value is None and self.default_value is not None):
-                return self._get_default_value()
-            return value
-        else:
-            return self._cache.get(instance)
+        key = f"{instance.name}__{self.name}"
+        value = self.handler(instance).get(key, UNSET)
+        if value is UNSET or (value is None and self.default_value is not None):
+            return self.get_default_value()
+        return value
 
     def __set__(self, instance, value):
         if not instance:
             raise NoInstanceError(f"{self.name} can only be set on instances.")
 
-        host = instance.host
-        key = f"{instance.name}_{self.name}"
-        self._cache[instance] = value
-        if host:
-            if not value or value == self._get_default_value():
-                current_value = self.handler(instance).get(key, UNSET)
-                if current_value is not UNSET:
-                    self.handler(instance).remove(key)
-            elif value:
-                self.handler(instance).add(key, value)
-
-        # TODO This could be taken from a constructor parameter or just erased
-        # TODO Since using setters would remove the need of such a thing
-        # TODO Also, the attribute handler already has something similar?
-        # signal = f'on_change_{self.name}'
-        # signal_func = getattr(instance, signal, None)
-        # if signal_func:
-        #     signal_func()
+        key = f"{instance.name}__{self.name}"
+        if not value or value == self.get_default_value():
+            current_value = self.handler(instance).get(key, UNSET)
+            if current_value is not UNSET:
+                self.handler(instance).remove(key)
+        elif value:
+            self.handler(instance).add(key, value)
 
     def __set_name__(self, owner, name):
+        """ This ensures Components are aware of their db fields """
         self.name = name
+        db_fields = getattr(owner, "_db_fields", None)
+        if db_fields is None:
+            db_fields = {}
+            setattr(owner, '_db_fields', db_fields)
+        db_fields[name] = self
 
     def __delete__(self, instance):
         if not instance:
@@ -82,10 +61,8 @@ class DBField(object):
 
         host = instance.host
         if host:
-            key = f"{instance.name}_{self.name}"
+            key = f"{instance.name}__{self.name}"
             self.handler(instance).remove(key)
-
-        del self._cache[instance]
 
     @classmethod
     def handler(cls, instance):
@@ -98,6 +75,13 @@ class NDBField(DBField):
     def handler(cls, instance):
         return instance.nattributes
 
+    def __set_name__(self, owner, name):
+        self.name = name
+        db_fields = getattr(owner, "_ndb_fields", None)
+        if db_fields is None:
+            db_fields = {}
+            setattr(owner, '_ndb_fields', db_fields)
+        db_fields[name] = self
 
 
 class NoInstanceError(Exception):
