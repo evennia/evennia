aacb723dea5d8e08f663eb5975d30df975906705
ChrisLR <arzhul@gmail.com>
2021-11-17 16:57:56 -0500

    Added an exception for when components have not been found, to provide a meaningful message

--- evennia/contrib/components/__init__.py
+++ evennia/contrib/components/__init__.py
@@ -48,5 +48,10 @@ from evennia.contrib.components.holder import ComponentHolderMixin, ComponentPro
 
 def get_component_class(component_name):
     subclasses = Component.__subclasses__()
-    component_class = next((sc for sc in subclasses if sc.name == component_name))
+    component_class = next((sc for sc in subclasses if sc.name == component_name), None)
+    if component_class is None:
+        message = f"Component named {component_name} has not been found. " \
+                  f"Make sure it has been imported before being used."
+        raise Exception(message)
+
     return component_class
