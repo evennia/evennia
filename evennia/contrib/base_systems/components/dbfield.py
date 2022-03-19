6f6ceb231c1b35adf607477af26100482035dc35
ChrisLR <arzhul@gmail.com>
2022-03-19 19:10:51 -0400

    Added TagField for components

--- evennia/contrib/base_systems/components/dbfield.py
+++ evennia/contrib/base_systems/components/dbfield.py
@@ -52,3 +52,46 @@ class NDBField(NAttributeProperty):
             ndb_fields = {}
             setattr(owner, '_ndb_fields', ndb_fields)
         ndb_fields[name] = self
+
+
+class TagField:
+    """
+    Component Descriptor to add a tag to the host.
+    """
+    def __init__(self, default=None, enforce_single=False):
+        self._category_key = None
+        self._default = default
+        self._enforce_single = enforce_single
+
+    def __set_name__(self, owner, name):
+        """
+        Called when descriptor is first assigned to the class. It is called with
+        the name of the field.
+
+        """
+        self._category_key = f"{owner.name}__{name}"
+        tag_fields = getattr(owner, "_tag_fields", None)
+        if tag_fields is None:
+            tag_fields = {}
+            setattr(owner, '_tag_fields', tag_fields)
+        tag_fields[name] = self
+
+    def __get__(self, instance, owner):
+        tag_value = instance.host.tags.get(
+            default=self._default,
+            category=self._category_key,
+        )
+        return tag_value
+
+    def __set__(self, instance, value):
+        tag_handler = instance.host.tags
+        if self._enforce_single:
+            tag_handler.clear(category=self._category_key)
+
+        tag_handler.add(
+            key=self._key,
+            category=self._category_key,
+        )
+
+    def __delete__(self, instance):
+        instance.host.tags.clear(category=self._category_key)
