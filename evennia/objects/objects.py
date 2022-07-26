8b696ed2b58c61dba9c9493127243d8a10acde1f
Andrew Bastien <volundmush@gmail.com>
2022-07-26 12:29:40 -0400

    object.at_traverse() now passes the exit being traversed as a kwarg

--- evennia/objects/objects.py
+++ evennia/objects/objects.py
@@ -866,6 +866,7 @@ class DefaultObject(ObjectDB, metaclass=TypeclassBase):
 
         Keyword Args:
           Passed on to announce_move_to and announce_move_from hooks.
+          Exits will set the "exit" kwarg to themselves.
 
         Returns:
             result (bool): True/False depending on if there were problems with the move.
@@ -2889,7 +2890,7 @@ class DefaultExit(DefaultObject):
 
         """
         source_location = traversing_object.location
-        if traversing_object.move_to(target_location, move_type="traverse"):
+        if traversing_object.move_to(target_location, move_type="traverse", exit=self):
             self.at_post_traverse(traversing_object, source_location)
         else:
             if self.db.err_traverse:
