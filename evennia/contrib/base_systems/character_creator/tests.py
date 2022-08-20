f3589e5f0378ebdda0ba9b83e8cbaf484ab572a5
InspectorCaracal <cal@clockworkcaracal.com>
2022-08-19 23:11:28 -0600

    ready to try it

--- evennia/contrib/base_systems/character_creator/tests.py
+++ evennia/contrib/base_systems/character_creator/tests.py
@@ -1,8 +1,6 @@
-from evennia.utils.test_resources import (
-    BaseEvenniaTest,
-    BaseEvenniaCommandTest,
-    EvenniaCommandTest,
-)  # noqa
+from django.test import override_settings
+from evennia.utils import inherits_from
+from evennia.utils.test_resources import BaseEvenniaCommandTest
 from . import character_creator
 
 class TestAccount(BaseEvenniaCommandTest):
@@ -12,21 +10,26 @@ class TestAccount(BaseEvenniaCommandTest):
                 account.CmdOOCLook(), "", "You are out-of-character (OOC).", caller=self.account
             )
         if settings.MULTISESSION_MODE == 2:
-						# with no playable characters
-            self.call(
+            # test both normal output and also inclusion of in-progress character
+            account.db._playable_characters = [self.char1]
+            self.char1.db.chargen_step = "start"
+            output = self.call(
                 account.CmdOOCLook(),
                 "",
                 "Account TestAccount (you are Out-of-Character)",
                 caller=self.account,
             )
-						# with in-progress character
+            self.assertIn("|Yin progress|n", output)
 
-
+    @override_settings(CHARGEN_MENU="evennia.contrib.base_systems.example_menu")
     def test_char_create(self):
+        account = self.account
+        session = self.session
         self.call(
             character_creator.ContribCmdCharCreate(),
-            "Test1=Test char",
-            "Created new character Test1. Use ic Test1 to enter the game",
-            caller=self.account,
+            "",
+            caller=account,
         )
-
+        menu = session.ndb._menutree
+        self.assertNotNone(menu)
+        self.assertTrue(inherits_from(session.new_char, DefaultCharacter) )
