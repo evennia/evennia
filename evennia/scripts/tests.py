d58985da66a9ef6b1a6e13f0660bf3a7f70713d5
henddher <henddher.pedroza@gmail.com>
2022-10-01 17:34:41 -0500

    Force failure.

--- evennia/scripts/tests.py
+++ evennia/scripts/tests.py
@@ -16,6 +16,7 @@ class TestScript(BaseEvenniaTest):
             self.assertTrue(obj, errors)
             self.assertFalse(errors, errors)
             mockinit.assert_called()
+            self.assertTrue(False)
 
 
 class TestScriptDB(TestCase):
