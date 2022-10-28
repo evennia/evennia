a3363b79c692b7695a5d8439e3c344b2d7570fa7
InspectorCaracal <cal@clockworkcaracal.com>
2022-10-28 09:44:11 -0600

    add docstring to broken script module

--- evennia/utils/tests/data/broken_script.py
+++ evennia/utils/tests/data/broken_script.py
@@ -1,3 +1,9 @@
+"""
+Defines a script module with a broken import, to catch the specific error case
+in loading global scripts where the module can be parsed but has broken
+dependencies.
+"""
+
 from evennia import nonexistent_module, DefaultScript
 
 class BrokenScript(DefaultScript):
