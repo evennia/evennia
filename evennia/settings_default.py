d3133a20210ec1f8b16ff65fd90eeb99c63d92f1
InspectorCaracal <51038201+InspectorCaracal@users.noreply.github.com>
2022-11-08 21:44:51 -0700

    Remove typeclass from default channel

--- evennia/settings_default.py
+++ evennia/settings_default.py
@@ -818,7 +818,6 @@ DEFAULT_CHANNELS = [
         "aliases": ("pub",),
         "desc": "Public discussion",
         "locks": "control:perm(Admin);listen:all();send:all()",
-        "typeclass": BASE_CHANNEL_TYPECLASS,
     }
 ]
 
