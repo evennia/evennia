87853b44a92ab69be1bc7a93f2a3f84d760a35c6
InspectorCaracal <51038201+InspectorCaracal@users.noreply.github.com>
2022-12-22 14:01:15 -0700

    fix regression

--- evennia/contrib/grid/xyzgrid/__init__.py
+++ evennia/contrib/grid/xyzgrid/__init__.py
@@ -2,14 +2,15 @@
 XYZGrid - Griatch 2021
 
 """
-
-from . import commands  # noqa
-from . import example  # noqa
-from . import launchcmd  # noqa
-from . import prototypes  # noqa
-from . import tests  # noqa
-from . import utils  # noqa
-from . import xymap  # noqa
-from . import xymap_legend  # noqa
-from . import xyzgrid  # noqa
-from . import xyzroom  # noqa
+from . import (
+    example,
+    launchcmd,
+    prototypes,
+    tests,
+    utils,
+    xymap,
+    xymap_legend,
+    xyzgrid,
+    xyzroom,
+    commands,
+)
