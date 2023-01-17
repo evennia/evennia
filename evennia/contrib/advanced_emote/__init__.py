d5a8e304af5f8dbb99110da661ee48538fd21929
Rhicora <rhicora@hotmail.com>
2023-01-17 12:26:58 -0800

    Install instructions

--- evennia/contrib/advanced_emote/__init__.py
+++ evennia/contrib/advanced_emote/__init__.py
@@ -1,6 +1,18 @@
 """
 Advanced Emote - Rhicora 2023
 
+To implement this, go to your character command set and add the line:
+
+from evennia.contrib.advanced_emote.actor_emote import CmdI
+
+Then make sure CmdI is available to PCs from creation:
+
+class PCCmdSet(CmdSet):
+    def at_cmdset_creation(self):
+        self.add(CmdI)
+
+Finally, make sure you have the dependency 'nltk', or things will break!
+
 """
 
 from .actor_emote import CmdI  # noqa
