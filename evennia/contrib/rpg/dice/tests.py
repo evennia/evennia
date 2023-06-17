df91d1860cb3ee045169a7719938e2f9e0e0f395
Griatch <griatch@gmail.com>
2023-06-17 18:53:48 +0200

    Refactor dice contrib

--- evennia/contrib/rpg/dice/tests.py
+++ evennia/contrib/rpg/dice/tests.py
@@ -3,9 +3,8 @@ Testing of TestDice.
 
 """
 
-from mock import patch
-
 from evennia.commands.default.tests import BaseEvenniaCommandTest
+from mock import patch
 
 from . import dice
 
@@ -13,9 +12,9 @@ from . import dice
 @patch("evennia.contrib.rpg.dice.dice.randint", return_value=5)
 class TestDice(BaseEvenniaCommandTest):
     def test_roll_dice(self, mocked_randint):
-        self.assertEqual(dice.roll_dice(6, 6, modifier=("+", 4)), mocked_randint() * 6 + 4)
-        self.assertEqual(dice.roll_dice(6, 6, conditional=("<", 35)), True)
-        self.assertEqual(dice.roll_dice(6, 6, conditional=(">", 33)), False)
+        self.assertEqual(dice.roll(6, 6, modifier=("+", 4)), mocked_randint() * 6 + 4)
+        self.assertEqual(dice.roll(6, 6, conditional=("<", 35)), True)
+        self.assertEqual(dice.roll(6, 6, conditional=(">", 33)), False)
 
     def test_cmddice(self, mocked_randint):
         self.call(
@@ -23,3 +22,15 @@ class TestDice(BaseEvenniaCommandTest):
         )
         self.call(dice.CmdDice(), "100000d1000", "The maximum roll allowed is 10000d10000.")
         self.call(dice.CmdDice(), "/secret 3d6 + 4", "You roll 3d6 + 4 (secret, not echoed).")
+
+    def test_string_form(self, mocked_randint):
+        self.assertEqual(dice.roll("6d6 + 4"), mocked_randint() * 6 + 4)
+        self.assertEqual(dice.roll("6d6 < 35"), True)
+        self.assertEqual(dice.roll("6d6 > 35"), False)
+        self.assertEqual(dice.roll("2d10 + 5 >= 14"), True)
+
+    def test_maxvals(self, mocked_randint):
+        with self.assertRaises(TypeError):
+            dice.roll(11, 1001, max_dicenum=10, max_dicetype=1000)
+        with self.assertRaises(TypeError):
+            dice.roll(10, 1001, max_dicenum=10, max_dicetype=1000)
