2f98d81d23aea3064ba352fe3e2491c2f1f5255a
Griatch <griatch@gmail.com>
2023-07-15 01:44:33 +0200

    Simplify LLM params, give OpenAI API example

--- evennia/contrib/rpg/llm/llm_client.py
+++ evennia/contrib/rpg/llm/llm_client.py
@@ -38,36 +38,8 @@ DEFAULT_LLM_PATH = "/api/v1/generate"
 DEFAULT_LLM_HEADERS = {"Content-Type": "application/json"}
 DEFAULT_LLM_PROMPT_KEYNAME = "prompt"
 DEFAULT_LLM_REQUEST_BODY = {
-    "max_new_tokens": 250,
-    # Generation params. If 'preset' is set to different than 'None', the values
-    # in presets/preset-name.yaml are used instead of the individual numbers.
-    "preset": "None",
-    "do_sample": True,
-    "temperature": 0.7,
-    "top_p": 0.1,
-    "typical_p": 1,
-    "epsilon_cutoff": 0,  # In units of 1e-4
-    "eta_cutoff": 0,  # In units of 1e-4
-    "tfs": 1,
-    "top_a": 0,
-    "repetition_penalty": 1.18,
-    "repetition_penalty_range": 0,
-    "top_k": 40,
-    "min_length": 0,
-    "no_repeat_ngram_size": 0,
-    "num_beams": 1,
-    "penalty_alpha": 0,
-    "length_penalty": 1,
-    "early_stopping": False,
-    "mirostat_mode": 0,
-    "mirostat_tau": 5,
-    "mirostat_eta": 0.1,
-    "seed": -1,
-    "add_bos_token": True,
-    "truncation_length": 2048,
-    "ban_eos_token": False,
-    "skip_special_tokens": True,
-    "stopping_strings": [],
+    "max_new_tokens": 250,  # max number of tokens to generate
+    "temperature": 0.7,  # higher = more random, lower = more predictable
 }
 
 
