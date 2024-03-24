a1023ebc7ee833904bf66107a92b9c1749afaf49
Griatch <griatch@gmail.com>
2024-03-24 17:23:04 +0100

    Add buff-contrib's exportable resources to outer package for import direct from the buff package.

---
+++ evennia/contrib/rpg/buffs/__init__.py
@@ -0,0 +1,4 @@
+from .buff import (BaseBuff, BuffableProperty, BuffHandler, CmdBuff,  # noqa
+                   Mod, cleanup_buffs, tick_buff)
+from .samplebuffs import (Exploit, Exploited, Leeching, Poison, Sated,  # noqa
+                          StatBuff)
--- evennia/contrib/rpg/llm/__init__.py
+++ evennia/contrib/rpg/llm/__init__.py
@@ -1,2 +1,2 @@
-from .llm_client import LLMClient
-from .llm_npc import LLMNPC, CmdLLMTalk
+from .llm_client import LLMClient  # noqa
+from .llm_npc import LLMNPC, CmdLLMTalk  # noqa
