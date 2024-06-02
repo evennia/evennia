ee92c3fd86d2ea89f27f1fb0455831b656f7e781
Cal <cal@clockworkcaracal.com>
2024-06-02 17:06:09 -0600

    catch WIP characters in IC command

--- evennia/contrib/rpg/character_creator/character_creator.py
+++ evennia/contrib/rpg/character_creator/character_creator.py
@@ -23,9 +23,11 @@ from django.conf import settings
 
 from evennia import DefaultAccount
 from evennia.commands.default.muxcommand import MuxAccountCommand
+from evennia.commands.default.account import CmdIC
+from evennia.commands.cmdset import CmdSet
 from evennia.objects.models import ObjectDB
 from evennia.utils.evmenu import EvMenu
-from evennia.utils.utils import is_iter
+from evennia.utils.utils import is_iter, string_partial_matching
 
 _MAX_NR_CHARACTERS = settings.MAX_NR_CHARACTERS
 
@@ -33,6 +35,17 @@ try:
     _CHARGEN_MENU = settings.CHARGEN_MENU
 except AttributeError:
     _CHARGEN_MENU = "evennia.contrib.rpg.character_creator.example_menu"
+
+
+class ContribCmdIC(CmdIC):
+    def func(self):
+        if self.args:
+            # check if the args match an in-progress character
+            wips = [chara for chara in self.account.characters if chara.db.chargen_step]
+            if matches := string_partial_matching([c.key for c in wips], self.args):
+                # the character is in progress, resume creation
+                return self.execute_cmd("charcreate")
+        super().func()
 
 
 class ContribCmdCharCreate(MuxAccountCommand):
@@ -94,6 +107,15 @@ class ContribCmdCharCreate(MuxAccountCommand):
                 account.execute_cmd("ic {}".format(char.key), session=session)
 
         EvMenu(session, _CHARGEN_MENU, startnode=startnode, cmd_on_exit=finish_char_callback)
+
+
+class ContribChargenCmdSet(CmdSet):
+    key = "Contrib Chargen CmdSet"
+
+    def at_cmdset_creation(self):
+        super().at_cmdset_creation()
+        self.add(ContribCmdIC)
+        self.add(ContribCmdCharCreate)
 
 
 class ContribChargenAccount(DefaultAccount):
