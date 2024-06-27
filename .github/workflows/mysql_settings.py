d54886e9e4b5308598f6784d69dfbdd85ea6ad2b
Griatch <griatch@gmail.com>
2024-06-27 18:47:49 +0200

    Testing to streamline mysql setup more

--- .github/workflows/mysql_settings.py
+++ .github/workflows/mysql_settings.py
@@ -46,10 +46,10 @@ DATABASES = {
         "PASSWORD": "evennia",
         "HOST": os.environ.get("MYSQL_HOST", "127.0.0.1"),
         "PORT": os.environ.get("MYSQL_PORT", "3306"),
-#         "OPTIONS": {
-#             "charset": "utf8mb4",
-#             "init_command": "set collation_connection=utf8mb4_unicode_ci",
-#         },
+        "OPTIONS": {
+             "charset": "utf8mb4",
+  #          "init_command": "set collation_connection=utf8mb4_unicode_ci",
+         },
 #         "TEST": {
 #             "NAME": "evennia",
 #             "OPTIONS": {
