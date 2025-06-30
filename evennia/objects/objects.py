671cab5cfdee5666b2571bca15878a04b5edf585
Griatch <griatch@gmail.com>
2025-06-30 17:04:18 +0200

    Send move_type to at_pre_object_* hooks

--- evennia/objects/objects.py
+++ evennia/objects/objects.py
@@ -1267,7 +1267,7 @@ class DefaultObject(ObjectDB, metaclass=TypeclassBase):
             # check if source location lets us go
             try:
                 if source_location and not source_location.at_pre_object_leave(
-                    self, destination, **kwargs
+                    self, destination, move_type=move_type, **kwargs
                 ):
                     return False
             except Exception as err:
@@ -1276,7 +1276,7 @@ class DefaultObject(ObjectDB, metaclass=TypeclassBase):
             # check if destination accepts us
             try:
                 if destination and not destination.at_pre_object_receive(
-                    self, source_location, **kwargs
+                    self, source_location, move_type=move_type, **kwargs
                 ):
                     return False
             except Exception as err:
