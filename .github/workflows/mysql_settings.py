99229c7de86148034bc4d09feb574c2d0f827df0
Griatch <griatch@gmail.com>
2025-12-22 10:33:09 +0100

    CI: Test setting mysql dynamic row format via django

--- .github/workflows/mysql_settings.py
+++ .github/workflows/mysql_settings.py
@@ -53,7 +53,8 @@ DATABASES = {
             # variables that don't require special privileges.
             "init_command": (
                 "SET collation_connection=utf8mb4_unicode_ci, "
-                "sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1"
+                "sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+                "innodb_default_row_format=DYNAMIC",
             ),
         },
         "TEST": {
@@ -62,7 +63,8 @@ DATABASES = {
                 "charset": "utf8mb4",
                 "init_command": (
                     "SET collation_connection=utf8mb4_unicode_ci, "
-                    "sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1"
+                    "sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+                    "innodb_default_row_format=DYNAMIC",
                 ),
             },
         },
